05e06460d56f052a82dd213175d58ae01e571a30
pmmaster <santisook.k@gmail.com>
2012-12-24 12:09:39 +0700

    fix     - try new method of rewrite add  - mock service for test purpose

---
+++ application/controllers/service.php
@@ -0,0 +1,41 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH.'/libraries/REST_Controller.php';
+
+class Service extends REST_Controller{
+
+	public function __construct(){
+		parent::__construct();
+	}
+
+	public function test_get(){
+		$data = array(
+			'status'	=> true,
+			'message'	=> 'simple REST service',
+			'time'		=> date('r e'),
+			'timestamp'	=> time(),
+		);
+
+		$this->response($data,200);
+	}
+
+	public function users_get(){
+		$data = array(
+			'user'	=> array(
+				array(
+					'name'	=> 'PM Master',
+					'age'	=> 24,
+				),
+				array(
+					'name'	=> 'Aquario',
+					'age'	=> 24,
+				),
+			),
+			'status'	=> true,
+			'time'		=> date('r e'),
+			'timestamp'	=> time(),
+
+			$this->response($data,200);
+		);
+	}
+}
