b64b12c0827435397e01bf9584d04b638fb972f1
pmmaster <santisook.k@gmail.com>
2012-12-26 13:42:05 +0700

    add - mobile services

---
+++ application/controllers/mobile.php
@@ -0,0 +1,38 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH.'/libraries/REST_Controller.php';
+
+class Mobile extends REST_Controller{
+	public function __construct(){
+		parent::__construct();
+	}
+
+	//mock mobile request
+	public function checkin(){
+		
+		$data = array(
+			'status'	=> true,
+			'message'	=> 'success',
+			'data'		=> array(
+				'image'		=>,
+				'text_message'	=> 'some message',
+				'post'		=> ,
+			),
+		);
+
+		$post = $this->input->post();
+
+		if(!isset($post['keyword']) || empty($post['keyword'])){
+			$data['status']		= false;
+			$data['message']	= 'keyword required';
+			$data['data']		= null;
+		}
+
+		switch ($post['keyword']) {
+			//select image here
+		}
+
+		//response
+		$this->response($data,200);
+	}
+}
