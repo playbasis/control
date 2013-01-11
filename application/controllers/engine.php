289810762aa0653e1ce34e4fdaa255764a93de07
pmmaster <santisook.k@gmail.com>
2013-01-11 11:18:07 +0700

    add engine

---
+++ application/controllers/engine.php
@@ -0,0 +1,27 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH.'/libraries/REST_Controller.php';
+class Engine extends REST_Controller{
+	public function __construct(){
+		parent::__construct();
+
+		//model
+
+
+
+		//library
+
+
+
+		//config
+	}
+
+	public function init(){
+
+	}
+
+	public function rule(){
+
+	}
+}
+?>
