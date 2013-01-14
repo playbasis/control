4ad913dcc79336903c1358428162648574f67749
eddie <eddie@playbasis.com>
2013-01-14 12:07:11 +0700

    fix client model

--- application/models/client_model.php
+++ application/models/client_model.php
@@ -93,5 +93,40 @@ class Client_model extends CI_Model{
 			$this->db->insert('playbasis_badge',array('pb_player_id'=>$pbPlayerId,'badge_id'=>$badgeId,'amount'=>$quantity,'date_added'=>date('Y-m-d H:i:s')));
 		}
 	}
+
+	public function updateExpAndLevel($exp,$pb_player_id){
+		assert($exp);
+		assert($pb_player_id);
+
+		//get level
+		$this->db->select_max('level');
+		$this->db->where("exp <=",$exp);
+		$result = $this->db->get('playbasis_exp_table');
+
+		$level = -1;
+		if($result->num_rows()){
+			$level = $result->row_array();
+			$level = $level['level'];
+		}
+		
+		$this->db->where('pb_player_id',$pb_player_id);
+		$this->db->set('exp', "exp+$exp", FALSE);
+		if($level>0)
+			$this->db->set('level', $level['level']);
+		
+		$this->db->update('playbasis_player');
+
+		return $level;			
+	}
+
+	public function log($logData){
+		assert($logData);
+		assert(is_array($logData));
+		assert($logData['pb_player_id']);
+		assert($logData['action_id']);
+		assert($logData['client_id']);
+		assert($logData['site_id']);
+		assert($logData['domain_name']);
+	} 
 }
 ?>
