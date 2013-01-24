02d6daaa808b11174fb648c0f4c79a903423636a
pmmaster <santisook.k@gmail.com>
2013-01-24 15:07:56 +0700

    fix database config

--- application/config/database.php
+++ application/config/database.php
@@ -64,7 +64,8 @@ $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
 
-$db['developer']['hostname'] = 'localhost';
+
+$db['developer']['hostname'] = 'pbapp.net';
 $db['developer']['username'] = 'playbasis_admin';
 $db['developer']['password'] = 'pl@yb@sis';
 $db['developer']['database'] = 'core';
