e37c84afa29132f80d4b495277fd4e2121ee6774
pmmaster <santisook.k@gmail.com>
2013-01-25 07:15:54 +0700

    add mock up message

--- application/models/tool/utility.php
+++ application/models/tool/utility.php
@@ -2,13 +2,14 @@
 class Utility extends CI_Model{
 	public function getEventMessage($eventType){
 		switch($eventType){
-			case 'badge'	:
-			case 'exp'		:
-			case 'point'	:
-			case 'level'	:
-			case 'login'	:
+			case 'badge'	:	return 'earned new badge'; break;
+			case 'exp'		:   return 'earned some exp'; break;
+			case 'point'	:	return 'earned some point'; break;
+			case 'level'	:	return 'earned new level'; break;
+			case 'login'	:   return 'logged in'; break;
+			case 'logout'	:   return 'logged out'; break;
 			default :
-				return '';
+				return 'this is default message';
 		}
 	}
 }
