86b5c7476038265a247b2140da9d6b1cf9fb34da
pmmaster <santisook.k@gmail.com>
2013-01-25 07:18:23 +0700

    add data formater

--- application/models/tool/node_stream.php
+++ application/models/tool/node_stream.php
@@ -1,7 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-// define('STREAM_URL', 'http://dev.pbapp.net/activitystream/');
-define('STREAM_URL', 'http://localhost/activitystream/');
+define('STREAM_URL', 'http://dev.pbapp.net/activitystream/');
 define('STREAM_PORT', 3000);
 define('USERPASS','planescape:torment');
 
@@ -12,22 +11,20 @@ class Node_stream extends CI_Model{
 
 		//set curl
 		$ch = curl_init();
-
-		var_dump(json_encode($data));
+		
 		//set curl option
 		curl_setopt($ch, CURLOPT_URL, STREAM_URL.$chanelName);											# set url
 		curl_setopt($ch, CURLOPT_PORT, STREAM_PORT);													# set port
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);    											# refuse response from called server
-		// curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8'));	# set Content-Type
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/plain; charset=utf-8'));	# set Content-Type
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, FALSE);    											# refuse response from called server
+		// curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8'));# set Content-Type
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/plain; charset=utf-8'));			# set Content-Type
     	curl_setopt($ch, CURLOPT_USERAGENT, 'CURL AGENT');												# set  agent    	
     	curl_setopt($ch, CURLOPT_TIMEOUT, 10); 															# time for execute
  		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,3);														# time for try to connect 
     	curl_setopt($ch, CURLOPT_POST, TRUE);															# use POST 
-    	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));										# wrap data 
-    	curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);											# http authen
+    	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($this->activityFeedFormatter($data)));			# wrap data 
+    	curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);												# http authen
     	curl_setopt($ch, CURLOPT_USERPWD, USERPASS);													# http authrn user password
-    	// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
 
     	//process 
     	curl_exec($ch);
@@ -41,6 +38,41 @@ class Node_stream extends CI_Model{
 
     	//close connection
     	curl_close($ch);
-	}	
+	}
+
+	private function activityFeedFormatter($data){
+		
+		$playerData = $this->getPlayerInfo($data['pb_player_id']);
+
+		$activityFormat = array(
+			'published'	=> date('c'), //rfc3339 , atom , ISO 8601
+			'actor'		=> array(
+							'objectType'	=> 'person',
+							'id'			=>	$playerData['cl_player_id'],
+							'displayName'	=>  $playerData['first_name'] . ' ' . $playerData['last_name'],
+							'image'			=> array(
+													'url'	=> $playerData['image'],
+													'width'	=> 150,
+													'height'=> 150	
+												),	
+						),
+			'verb'		=> $data['action_name'],
+			'object'	=> array(
+							'message'		=> $data['message'],
+						),
+			'target'	=> array(),
+		);
+
+		return $activityFormat;
+
+	}
+
+	private function getPlayerInfo($pb_player_id){
+		$this->db->select('cl_player_id,first_name,last_name,image');
+		$this->db->where('pb_player_id',$pb_player_id);
+		$result = $this->db->get('playbasis_player');
+
+		return $result->row_array();
+	}
 }
 ?>
