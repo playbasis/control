56d38f5cda349ecd375ab12f2e66be466b420f29
pmmaster <santisook.k@gmail.com>
2013-01-28 14:15:45 +0700

    just a debug test

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -32,7 +32,11 @@ class Auth extends REST_Controller{
 		#get client relate to api key 
 		$clientInfo = $this->auth_model->getApiInfo($API);
 
-		$this->response($clientInfo,200);
+		var_dump($this->input->post());
+		var_dump($clientInfo);
+		// $this->response($clientInfo,200);
+		die();
+		
 		#validate 
 		if($clientInfo){
 			//gen token
