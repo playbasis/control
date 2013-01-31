7cdcfd189a33ce4e80c6651c9a9624e3a176e116
eddie <eddie@playbasis.com>
2013-01-31 13:18:00 +0700

    optimize node server to pass feed data as raw string

--- node_server/app.js
+++ node_server/app.js
@@ -1,35 +1,51 @@
 
 var REDIS_SERVER_PORT = 6379;
 var REDIS_SERVER_ADDRESS = '127.0.0.1';
-
+var METHOD_PUBLISH_FEED = '/activitystream';
 /**
  * Module dependencies.
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path')
-  , io = require('socket.io')
-  , redis = require('redis');
+	, routes = require('./routes')
+	, user = require('./routes/user')
+	, http = require('http')
+	, path = require('path')
+	, io = require('socket.io')
+	, redis = require('redis');
 
 var app = express();
 
+//special parser for the activity feed
+function feedParser(req, res, next){
+	if(req.originalUrl.substr(0, METHOD_PUBLISH_FEED.length).toLowerCase() != METHOD_PUBLISH_FEED)
+		return next();
+	var data = '';
+	req.setEncoding('utf8');
+	req.on('data', function(chunk){
+		data += chunk;
+	});
+	req.on('end', function(){
+		req.body = data;
+		next();
+	})
+}
+
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(feedParser);
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 });
 
 app.get('/', routes.index);
@@ -38,51 +54,51 @@ app.get('/users', user.list);
 var server = http.createServer(app);
 io = io.listen(server);
 server.listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+	console.log("Express server listening on port " + app.get('port'));
 });
 
 var redisSubClients = Object(); //an object holding redis clients that subscribed to a channel
 var redisPubClient = redis.createClient(); //redis client for publishing feeds
 
 redisPubClient.on('error', function(err){
-  console.log('redis pub-client err: ' + err);
+	console.log('redis pub-client err: ' + err);
 });
 
 io.sockets.on('connection', function(socket){
 
-  socket.on('subscribe', function(data){
+	socket.on('subscribe', function(data){
 
-    if(!data.channel)
-      return;
-      
-    if(!redisSubClients[data.channel]){
-      createRedisSubClient(data.channel);
-      //assert(redisSubClients[data.channel]);
-      redisSubClients[data.channel].subscribe(data.channel);
+		if(!data.channel)
+			return;
+			
+		if(!redisSubClients[data.channel]){
+			createRedisSubClient(data.channel);
+			//assert(redisSubClients[data.channel]);
+			redisSubClients[data.channel].subscribe(data.channel);
 
-      //set callback to emit message to connected clients
-      redisSubClients[data.channel].on('message', function(channel, message){
-        io.sockets.in(channel).emit('message', message);
-      });
-    }
-    socket.join(data.channel);
-  });
+			//set callback to emit message to connected clients
+			redisSubClients[data.channel].on('message', function(channel, message){
+				io.sockets.in(channel).emit('message', message);
+			});
+		}
+		socket.join(data.channel);
+	});
 });
 
 //publish event through post request
-app.post('/activitystream/:channel', function(req, res){
-  if(req.body)
-    redisPubClient.publish(req.params.channel, JSON.stringify(req.body));
-  res.send(200);
+app.post(METHOD_PUBLISH_FEED + '/:channel', function(req, res){
+	if(req.body)
+		redisPubClient.publish(req.params.channel, req.body);
+	res.send(200);
 });
 
 //create redis sub-client for the specified channel
 function createRedisSubClient(channel){
 
-  //assert(!redisSubClients[channel]);
-  redisSubClients[channel] = redis.createClient(REDIS_SERVER_PORT, REDIS_SERVER_ADDRESS);
+	//assert(!redisSubClients[channel]);
+	redisSubClients[channel] = redis.createClient(REDIS_SERVER_PORT, REDIS_SERVER_ADDRESS);
 
-  redisSubClients[channel].on('error', function(err){
-    console.log('redis sub-client err: ' + err);
-  });
+	redisSubClients[channel].on('error', function(err){
+		console.log('redis sub-client err: ' + err);
+	});
 }
