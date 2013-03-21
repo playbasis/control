51a22c5f089d0b3f91bcfca7cf6bb17dc1bbc8f4
eddie <eddie@playbasis.com>
2013-03-21 18:36:25 +0700

    beautify respond.php

--- application/models/tool/respond.php
+++ application/models/tool/respond.php
@@ -1,19 +1,20 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-class Respond extends CI_Model{
-	public function __construct(){
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Respond extends CI_Model
+{
+	public function __construct()
+	{
 		parent::__construct();
 	}
-
-	public function setRespond($data=array()){
-		$respondData  = array();
-
-		$respondData['success'] 	= true;
-		$respondData['error_code'] 	= '0000';
-		$respondData['message'] 	= 'Success';
-		$respondData['response'] 	= $data;
-		$respondData['timestamp'] 	= (int)time();
-		$respondData['time'] 		= date('r e');
-
+	public function setRespond($data = array())
+	{
+		$respondData = array();
+		$respondData['success'] = true;
+		$respondData['error_code'] = '0000';
+		$respondData['message'] = 'Success';
+		$respondData['response'] = $data;
+		$respondData['timestamp'] = (int) time();
+		$respondData['time'] = date('r e');
 		return $respondData;
 	}
 }
