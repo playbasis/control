9eb71b06bc5dc25f5aaa674a42a2830328b0732b
eddie <eddie@playbasis.com>
2013-03-21 17:07:09 +0700

    beautify badge.php

--- application/controllers/badge.php
+++ application/controllers/badge.php
@@ -1,72 +1,67 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-require APPPATH.'/libraries/REST_Controller.php';
-class Badge extends REST_Controller{
-	public function __construct(){
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH . '/libraries/REST_Controller.php';
+class Badge extends REST_Controller
+{
+	public function __construct()
+	{
 		parent::__construct();
-
 		$this->load->model('auth_model');
 		$this->load->model('badge_model');
-		$this->load->model('tool/error','error');
-		$this->load->model('tool/respond','resp');
+		$this->load->model('tool/error', 'error');
+		$this->load->model('tool/respond', 'resp');
 	}
-
-
-	public function index_post($badgeId=0){
-		$required = $this->input->checkParam(array('token'));
-
+	public function index_post($badgeId = 0)
+	{
+		$required = $this->input->checkParam(array(
+			'token'
+		));
 		if($required)
-			$this->response($this->error->setError('TOKEN_REQUIRED',$required),200);
-
+			$this->response($this->error->setError('TOKEN_REQUIRED', $required), 200);
 		//validate token
 		$validToken = $this->auth_model->findToken($this->input->post('token'));
-		
 		if(!$validToken)
-			$this->response($this->error->setError('INVALID_TOKEN'),200);
-
+			$this->response($this->error->setError('INVALID_TOKEN'), 200);
 		// $validToken = array('client_id'=>1,'site_id'=>1); //for debugging
-
-		
-		if($badgeId){
+		if($badgeId)
+		{
 			//get badge by specific id
-			$badge['badge'] = $this->badge_model->getBadge(array_merge($validToken,array('badge_id'=>$badgeId)));
-
-			$this->response($this->resp->setRespond($badge),200);
+			$badge['badge'] = $this->badge_model->getBadge(array_merge($validToken, array(
+				'badge_id' => $badgeId
+			)));
+			$this->response($this->resp->setRespond($badge), 200);
 		}
-		else{
+		else
+		{
 			//get all badge relate to  clients
 			$badgesList['badges'] = $this->badge_model->getAllBadges($validToken);
-
-			$this->response($this->resp->setRespond($badgesList),200);	
+			$this->response($this->resp->setRespond($badgesList), 200);
 		}
-		
 	}
-
-	public function getCollection_post($collectionId  = 0){
-		$required = $this->input->checkParam(array('token'));
-
+	public function getCollection_post($collectionId = 0)
+	{
+		$required = $this->input->checkParam(array(
+			'token'
+		));
 		if($required)
-			$this->response($this->error->setError('TOKEN_REQUIRED',$required),200);
-
+			$this->response($this->error->setError('TOKEN_REQUIRED', $required), 200);
 		//validate token
 		$validToken = $this->auth_model->findToken($this->input->post('token'));
-		
 		if(!$validToken)
-			$this->response($this->error->setError('INVALID_TOKEN'),200);
-
+			$this->response($this->error->setError('INVALID_TOKEN'), 200);
 		// $validToken = array('client_id'=>1,'site_id'=>1); //for debugging
-
-		if($collectionId){
+		if($collectionId)
+		{
 			//get collection by id
-
-			$collection['collection'] = $this->badge_model->getCollection(array_merge($validToken,array('collection_id'=>$collectionId)));
-
-			$this->response($this->resp->setRespond($collection),200); 
+			$collection['collection'] = $this->badge_model->getCollection(array_merge($validToken, array(
+				'collection_id' => $collectionId
+			)));
+			$this->response($this->resp->setRespond($collection), 200);
 		}
-		else{
+		else
+		{
 			$collectionsList['collections'] = $this->badge_model->getAllCollection($validToken);
-
-			$this->response($this->resp->setRespond($collectionsList),200);
+			$this->response($this->resp->setRespond($collectionsList), 200);
 		}
 	}
 }
