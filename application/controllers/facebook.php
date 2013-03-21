63d8d2f9dea04fdd0acf5065ba22ff859b53afc7
eddie <eddie@playbasis.com>
2013-03-21 18:56:04 +0700

    beautify facebook.php

--- application/controllers/facebook.php
+++ application/controllers/facebook.php
@@ -1,27 +1,24 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-require APPPATH.'/libraries/REST_Controller.php';
-require APPPATH.'/libraries/facebook-php-sdk/facebook.php';
-
-class Facebook extends REST_Controller{
-	
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH . '/libraries/REST_Controller.php';
+require APPPATH . '/libraries/facebook-php-sdk/facebook.php';
+class Facebook extends REST_Controller
+{
 	private $facebook = null;
-	
-	public function __construct(){
+	public function __construct()
+	{
 		parent::__construct();
-
 		$this->load->model('social_model');
 	}
-	
-	public function realtimeupdate_get(){
-					
+	public function realtimeupdate_get()
+	{
 		//$mode = $this->input->get('hub_mode');
 		//$verifyToken = $this->input->get('hub_verify_token');
 		$challenge = $this->input->get('hub_challenge');
 		echo $challenge;
 	}
-	
-	public function realtimeupdate_post(){
+	public function realtimeupdate_post()
+	{
 		echo $this->request->body;
 	}
 }
