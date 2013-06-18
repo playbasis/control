9b61f2c0ab30126100873c18a2550f450a324093
eddie <eddie@playbasis.com>
2013-06-18 18:55:21 +0700

    badge_to_client moved to mongo

--- application/models/badge_model.php
+++ application/models/badge_model.php
@@ -12,17 +12,17 @@ class Badge_model extends MY_Model
 	public function getAllBadges($data)
 	{
 		//get badge ids
-		$this->set_site_mongodb($data['site_id']);
-		$this->set_site($data['site_id']);
-		$this->site_db()->select('badge_id');
-		$this->site_db()->where(array(
-			'client_id' => $data['client_id'],
-			'site_id' => $data['site_id']
+		$this->set_site_mongodb($data['site_id']);		
+		$this->mongo_db->select(array('badge_id'));
+		$this->mongo_db->where(array(
+			'client_id' => intval($data['client_id']),
+			'site_id' => intval($data['site_id'])
 		));
-		$badgeSet = db_get_result_array($this, 'playbasis_badge_to_client');
+		$badgeSet = $this->mongo_db->get('badge_to_client');
 		if(!$badgeSet)
 			return array();
 		//get data on badges
+		$this->set_site($data['site_id']);
 		foreach($badgeSet as &$badge)
 		{
 			//get badge data
@@ -39,6 +39,7 @@ class Badge_model extends MY_Model
 			unset($result['_id']);
 			$result['image'] = $this->config->item('IMG_PATH') . $result['image'];
 			$badge = array_merge($badge, $result);
+			unset($badge['_id']);
 		}
 		return $badgeSet;
 	}
@@ -46,20 +47,22 @@ class Badge_model extends MY_Model
 	{
 		//get badge id
 		$this->set_site_mongodb($data['site_id']);
-		$this->set_site($data['site_id']);
-		$this->site_db()->select('badge_id');
-		$this->site_db()->where(array(
-			'client_id' => $data['client_id'],
-			'site_id' => $data['site_id'],
-			'badge_id' => $data['badge_id']
+		$this->mongo_db->select(array('badge_id'));
+		$this->mongo_db->where(array(
+			'client_id' => intval($data['client_id']),
+			'site_id' => intval($data['site_id']),
+			'badge_id' => intval($data['badge_id'])
 		));
-		$result = db_get_row_array($this, 'playbasis_badge_to_client');
-		if(!$result)
+		$result = $this->mongo_db->get('badge_to_client');
+		if(!$result || !$result[0])
 			return array();
+		$result = $result[0];
+		unset($result['_id']);
 		$badge = array(
 			'badge_id' => $data['badge_id']
 		);
 		//get badge data
+		$this->set_site($data['site_id']);
 		$this->site_db()->select('name,description,hint');
 		$this->site_db()->where('badge_id', $badge['badge_id']);
 		$result = db_get_row_array($this, 'playbasis_badge_description');
