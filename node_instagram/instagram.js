d95e673d4782388b002faeaf1adda362c5dc201f
eddie <eddie@playbasis.com>
2013-06-28 18:26:55 +0700

    node instagram update

--- node_instagram/instagram.js
+++ node_instagram/instagram.js
@@ -6,7 +6,7 @@
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
-  , http = require('https')
+  , http = require('http')
   , path = require('path')
   , fs = require('fs')
   , io = require('socket.io')
@@ -69,14 +69,14 @@ db.once('open', function callback(){
 console.log('connecting to db...');
 
 var options = {
-	key:  fs.readFileSync('/usr/bin/ssl/pbapp.net.key'),
-	cert: fs.readFileSync('/usr/bin/ssl/pbapp.net.crt'),
-	ca:   fs.readFileSync('/usr/bin/ssl/gd_bundle.crt'),
+	//key:  fs.readFileSync('/usr/bin/ssl/pbapp.net.key'),
+	//cert: fs.readFileSync('/usr/bin/ssl/pbapp.net.crt'),
+	//ca:   fs.readFileSync('/usr/bin/ssl/gd_bundle.crt'),
 	requestCert: true,
 	rejectUnauthorized: false
 };
 
-var server = http.createServer(options, app);
+var server = http.createServer(app);//(options, app);
 io = io.listen(server);
 server.listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
@@ -112,16 +112,16 @@ app.get('/subscribe/tag/:tag', auth, function(req, res)
 	 	var dateObj = new Date();
 		var meta = new IGMeta({
 			'tag_name': tag,
-			'last_feed_time': dateObj.getTime();
+			'last_feed_time': Math.round(dateObj.getTime() / 1000)
 		});
-		metas[tag].last_feed_time = meta.last_feed_time;
+		metas[tag] = { last_feed_time: meta.last_feed_time };
 		meta.save(function(err){
 			if(err){
 				console.log(err);
 			}
 		});
 	});
-	instagram.subscriptions.subscribe({ object : 'tag', object_id : tag });
+	//instagram.subscriptions.subscribe({ object : 'tag', object_id : tag });
 	res.send(200);
 });
 
@@ -144,6 +144,57 @@ app.get('/feed', function(req, res){
 	res.send(200);
 });
 
+function processRecentFeeds(tag)
+{
+	instagram.tags.recent({ name: tag, complete: function(data, pagination){
+		var datalen = data.length;
+		console.log('recents: ' + datalen);
+		var maxFeedTime = metas[tag].last_feed_time;
+		console.log('last feed time:');
+		console.log(maxFeedTime);
+		for(var i=0; i<datalen; ++i){
+			var feed = data[i];
+			//ignore old feeds
+			var feedTime = feed.created_time;
+			if(feedTime <= metas[tag].last_feed_time)
+			{
+				console.log('skipping: ' + feedTime);
+				continue;
+			}
+			if(feedTime > maxFeedTime)
+				maxFeedTime = feedTime;
+
+			//save new feed
+			console.log(feed.created_time);
+			console.log(feed.user.username);
+			
+			var entry = new IGFeed({
+				'user': feed.user.username,
+				'name': feed.user.full_name,
+				'id': feed.user.id,
+				'profile_image': feed.user.profile_picture,
+				'photo': feed.images.standard_resolution.url,
+				'thumbnail' : feed.images.thumbnail.url,
+				'caption': (feed.caption) ? feed.caption.text : '',
+				'tag': tag,
+				'time': feed.created_time
+			});
+			console.log('saving entry...');
+			entry.save(function(err){
+				if(err){
+					console.log(err);
+					return;
+				}
+				console.log('post saved!');
+				var dateObj = new Date();
+				//tell clients to update data
+				io.sockets.emit('newigpost', {'time': dateObj.getTime()});
+			});
+		}
+		metas[tag].last_feed_time = maxFeedTime;
+	}});
+}
+
 app.post('/feed', function(req, res)
 {
 	console.log('---------- ig post ----------');
@@ -156,59 +207,24 @@ app.post('/feed', function(req, res)
 	}
 	var body = req.body;
 	var bodylen = body.length;
+	console.log('update: ' + bodylen);
 	for(var i=0; i<bodylen; ++i){
 		var tag = body[i].object_id;
-		if(!metas.hasOwnProperty(tag))
-		{
+		console.log('tag: ' + tag);
+		if(!metas.hasOwnProperty(tag)){
+			console.log('finding tag: ' + tag);
 			IGMeta.findOne({ tag_name: tag }, function(err, meta){
-				metas[tag].last_feed_time = meta.last_feed_time;
+				if(meta){
+					metas[meta.tag_name] = { last_feed_time: meta.last_feed_time };
+					console.log('found tag in meta: ' + meta.tag_name + ' last feed time: ' + metas[meta.tag_name].last_feed_time);
+					processRecentFeeds(meta.tag_name);
+				}
+				else console.log('not found tag in meta: ' + tag.tag_name);
 			});
 			continue;
 		}
-		instagram.tags.recent({ name: tag, complete: function(data, pagination){
-			console.log('recents:');
-			var datalen = data.length;
-			var maxFeedTime = metas[tag].last_feed_time;
-			console.log('last feed time:');
-			console.log(maxFeedTime);
-			for(var j=0; j<datalen; ++j){
-				var feed = data[j];
-				//ignore old feeds
-				var feedTime = feed.created_time;
-				if(feedTime <= metas[tag].last_feed_time)
-					continue;
-				if(feedTime > maxFeedTime)
-					maxFeedTime = feedTime;
-
-				//save new feed
-				console.log(feed.created_time);
-				console.log(feed.user.username);
-				
-				var entry = new IGFeed({
-					'user': feed.user.username,
-					'name': feed.user.full_name,
-					'id': feed.user.id,
-					'profile_image': feed.user.profile_picture,
-					'photo': feed.images.standard_resolution.url,
-					'thumbnail' : feed.images.thumbnail.url,
-					'caption': (feed.caption) ? feed.caption.text : '',
-					'tag': tag,
-					'time': feed.created_time
-				});
-				console.log('saving entry...');
-				entry.save(function(err){
-					if(err){
-						console.log(err);
-						return;
-					}
-					console.log('post saved!');
-					var dateObj = new Date();
-					//tell clients to update data
-					io.sockets.emit('newigpost', {'time': dateObj.getTime()});
-				});
-			}
-			metas[tag].last_feed_time = maxFeedTime;
-		}});
+		console.log('start query recents: ' + tag);
+		processRecentFeeds(tag);
 	}
 	res.send(200);
 });
