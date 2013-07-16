2128f7b284e854ab03e5bafdc205aeb235bb1854
eddie <eddie@playbasis.com>
2013-07-16 16:54:03 +0700

    sharepoint WIP

---
+++ pblib/sharepoint/Playbasis/PlaybasisWeb/Services/Survey_ItemDeleted.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.EventReceivers;
+
+namespace PlaybasisWeb.Services
+{
+	public class Survey_ItemDeleted : IRemoteEventService
+	{
+		public SPRemoteEventResult ProcessEvent(SPRemoteEventProperties properties)
+		{
+			SPRemoteEventResult result = new SPRemoteEventResult();
+
+			using (ClientContext clientContext = TokenHelper.CreateRemoteEventReceiverClientContext(properties))
+			{
+				if (clientContext != null)
+				{
+					clientContext.Load(clientContext.Web);
+					clientContext.ExecuteQuery();
+				}
+			}
+
+			return result;
+		}
+
+		public void ProcessOneWayEvent(SPRemoteEventProperties properties)
+		{
+			TraceHelper.RemoteLog(properties.EventType.ToString());
+			TraceHelper.RemoteLog("list id: " + properties.ItemEventProperties.ListId.ToString());
+			TraceHelper.RemoteLog("item id: " + properties.ItemEventProperties.ListItemId.ToString());
+			TraceHelper.RemoteLog("user id: " + properties.ItemEventProperties.CurrentUserId.ToString());
+		}
+	}
+}
---
+++ pblib/sharepoint/Playbasis/PlaybasisWeb/TraceHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.SharePoint.Client.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace PlaybasisWeb
+{
+	public class TraceHelper
+	{
+		public static void RemoteLog(string message)
+		{
+			const string ECHO_URL = "https://node.pbapp.net:3004/echo/msg/";
+			WebClient client = new WebClient();
+			client.DownloadStringAsync(new Uri(ECHO_URL + HttpUtility.UrlPathEncode(message, true, true, true)));
+		}
+	}
+}
