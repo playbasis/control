e36173dc39c3c9dbff28dd229a02d466a16b8c9b
eddie <eddie@playbasis.com>
2013-07-24 18:06:51 +0700

    migrate point model to new mongodb

---
+++ application/controllers/point.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH . '/libraries/REST_Controller.php';
+class Point extends REST_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('auth_model');
+		$this->load->model('point_model');
+		$this->load->model('tool/error', 'error');
+		$this->load->model('tool/respond', 'resp');
+	}
+	public function test_get()
+	{
+		echo '<pre>';
+		$credential = array(
+			'key' => 'abc',
+			'secret' => 'abcde'
+		);
+		$token = $this->auth_model->getApiInfo($credential);
+		echo '<br>findPoint:<br>';
+		$result = $this->point_model->findPoint(array_merge($token, array('reward_name'=>'point')));
+		print_r($result);
+		echo '<br>getRewardNameById:<br>';
+		$result = $this->point_model->getRewardNameById(array_merge($token, array('reward_id'=>$result)));
+		print_r($result);
+		echo '</pre>';
+	}
+}
+?>
--- application/models/point_model.php
+++ application/models/point_model.php
@@ -14,11 +14,11 @@ class Point_model extends MY_Model
 		$this->set_site_mongodb($data['site_id']);
 		$this->mongo_db->select(array('name'));
 		$this->mongo_db->where(array(
-			'client_id' => intval($data['client_id']),
-			'site_id' => intval($data['site_id']),
-			'reward_id' => intval($data['reward_id'])
+			'client_id' => $data['client_id'],
+			'site_id' => $data['site_id'],
+			'reward_id' => $data['reward_id']
 		));
-		$result = $this->mongo_db->get('reward_to_client');
+		$result = $this->mongo_db->get('playbasis_reward_to_client');
 		return ($result) ? $result[0]['name'] : $result;
 	}
 	public function findPoint($data)
@@ -26,11 +26,11 @@ class Point_model extends MY_Model
 		$this->set_site_mongodb($data['site_id']);
 		$this->mongo_db->select(array('reward_id'));
 		$this->mongo_db->where(array(
-			'client_id' => intval($data['client_id']),
-			'site_id' => intval($data['site_id']),
+			'client_id' => $data['client_id'],
+			'site_id' => $data['site_id'],
 			'name' => strtolower($data['reward_name'])
 		));
-		$result = $this->mongo_db->get('reward_to_client');
+		$result = $this->mongo_db->get('playbasis_reward_to_client');
 		if($result)
 			return $result[0]['reward_id'];
 		return array();
