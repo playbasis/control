f035c1a00cd22fde7d9e8008150df0a49cca5dfb
Wee <wee_weerapat@hotmail.com>
2013-08-09 11:31:47 +0700

    update

--- application/config/routes.php
+++ application/config/routes.php
@@ -98,6 +98,7 @@ $route['Badges'] = 'badge/index';
 $route['Engine/actionConfig']	= 'engine/getActionConfig';
 $route['Engine/rule/'.ANY_STRING] = 'engine/rule/$1';
 $route['Engine/rule']	= 'engine/rule/0';
+$route['Engine/facebook']	= 'Engine/rule/facebook';
 
 //misc
 $route['test']	= 'playbasis/test';
