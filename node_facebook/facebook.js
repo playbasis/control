0087eddf001db495a7ece539501d1cfd4a0168fc
Wee <wee_weerapat@hotmail.com>
2013-08-09 15:38:50 +0700

    test facebook

---
+++ node_facebook/facebook.js
@@ -0,0 +1,67 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , io = require('socket.io');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3006);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+var options = {
+//	key:  fs.readFileSync('/usr/bin/ssl/pbapp.net.key'),
+//	cert: fs.readFileSync('/usr/bin/ssl/pbapp.net.crt'),
+//	ca:   fs.readFileSync('/usr/bin/ssl/gd_bundle.crt'),
+	requestCert: true,
+	rejectUnauthorized: false
+};
+
+//var server = http.createServer(options, app);
+var server = http.createServer(app);
+io = io.listen(server);
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+io.sockets.on('connection', function(socket){
+	console.log('new connection...');
+});
+
+app.get('/echo/:channel/:message', function(req, res){
+	var channel = req.params.channel;
+	var message = req.params.message;
+	io.sockets.emit(channel, {'msg': message});
+	res.send(200);
+});
+
+app.get('/facebook', function(req, res){
+    res.send(req.query['hub.challenge']);
+});
+
+app.post('/facebook', function(req, res){
+    console.log(req);
+    res.send(200);
+});
