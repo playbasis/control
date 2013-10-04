2d4ea9c14d5199a70557762d4549dcace6b868f8
Wee <wee_weerapat@hotmail.com>
2013-10-05 06:35:09 +0700

    incress speed of levels

--- application/models/level_model.php
+++ application/models/level_model.php
@@ -65,27 +65,31 @@ class Level_model extends MY_Model
     public function getLevelsDetail($client_id, $site_id)
     {
         $this->set_site($site_id);
-        $this->site_db()->select_min('level');
-        $slevel = db_get_row_array($this, 'playbasis_exp_table');
-        if(!isset($slevel['level'])){
-            $min_level = 1;
-        }else{
-            $min_level = intval($slevel['level']);
+        //check if client have their own exp table setup
+        $this->site_db()->select("level,exp AS min_exp,level_title,image");
+        $this->site_db()->where('client_id', $client_id);
+        $this->site_db()->where('site_id', $site_id);
+        $this->site_db()->where("status", '1');
+        $this->site_db()->order_by('exp');
+        $leveldata = db_get_result_array($this, 'playbasis_client_exp_table');
+
+        if(!isset($level['level']))
+        {
+            //get level from default exp table instead
+            $this->site_db()->select("level,exp AS min_exp,level_title,image");
+            $this->site_db()->where("status", '1');
+            $this->site_db()->order_by('exp');
+            $leveldata = db_get_result_array($this, 'playbasis_exp_table');
+
         }
 
-        $this->site_db()->select_max('level');
-        $slevel = db_get_row_array($this, 'playbasis_exp_table');
-        if(!isset($slevel['level'])){
-            $max_level = 100;
-        }else{
-            $max_level = intval($slevel['level']);
+        $i = 0;
+        foreach($leveldata as &$l){
+            $l['max_exp'] = (isset($leveldata[$i+1]))?intval($leveldata[$i+1]['min_exp'])-1:null;
+            $i++;
         }
 
-        for($i = $min_level; $i <= $max_level; $i++){
-            $level[] = $this->getLevelDetail($i, $client_id, $site_id);
-        }
-
-        return $level;
+        return $leveldata;
     }
 }
 ?>
