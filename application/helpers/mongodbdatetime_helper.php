d31607c3ab27981972bb09fdf859f7d349acacb8
Joe Prasetyo <Joe@Wees-Mac-mini.local>
2013-12-04 10:39:27 +0700

    Added date time helper for date conversation

---
+++ application/helpers/mongodbdatetime_helper.php
@@ -0,0 +1,17 @@
+<?php
+  function datetimeMongotoReadable($dateTimeMongo)
+    {
+        if ($dateTimeMongo) {
+            if (isset($dateTimeMongo->sec)) {
+                $dateTimeMongo = date("Y-m-d H:i:s", $dateTimeMongo->sec);
+            } else {
+                $dateTimeMongo = $dateTimeMongo;
+            }
+        } else {
+            $dateTimeMongo = "0000-00-00 00:00:00";
+        }
+        return $dateTimeMongo;
+    }
+
+ ?>
+
