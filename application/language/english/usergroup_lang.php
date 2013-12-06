697add67926e164a8ff6f081ecfc3fc99fbfb153
Joe Prasetyo <joe@playbasis.com>
2013-12-06 17:47:59 +0700

    Created new file - usergoup_lang.php file

---
+++ application/language/english/usergroup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+//Heading
+$lang['heading_title']				= "User Group";
+$lang['title']						= "User Group";
+
+// Column
+$lang['column_name']				= 'Name';
+$lang['column_status']				= 'Status';
+$lang['column_action']				= 'Action';
+
+// Error 
+$lang['error_warning']          	= 'Warning: Please check the form carefully for errors!';
+//$lang['error_permission']       	= 'Warning: You do not have permission to modify clients!';
+//$lang['error_name']             	= 'Category Name must be between 2 and 32 characters!';
+//$lang['error_required']			= 'Warning: Please check required field!!';
+//$lang['error_user']				= 'Error: Username not availble!!!';
+
+//Text
+//$lang['text_no_results']          = 'Result Not found.';
+//$lang['text_edit']                = 'Edit';
+$lang['text_success']           	= 'Success: You have modified a user!';
+//$lang['text_default']           	= 'Default';
+//$lang['text_image_manager']     	= 'Image Manager';
+//$lang['text_browse']            	= 'Browse Files';
+//$lang['text_clear']             	= 'Clear Image';
+//$lang['text_criteria']          	= 'Domain View';
+//$lang['text_confirm']			    = 'Remove cannot be undone! Are you sure you want to do this?';
+
+// Button
+$lang['button_filter']				= 'Filter';
+$lang['button_insert']				= "Insert";
+$lang['button_delete']				= "Delete";
+
+
