40919b72d8d680795270e11f28640eafa750b4c7
wee <wee@playbasis.com>
2013-12-06 15:02:05 +0700

    clean code

--- application/config/config-sample.php
+++ application/config/config-sample.php
@@ -1,4 +1,4 @@
-mongodb.php<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /*
 |--------------------------------------------------------------------------
