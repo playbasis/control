dddb558511c351dd8c690e444d39840beec9e640
Joe Prasetyo <joe@playbasis.com>
2013-12-09 14:26:18 +0700

    added fetchAllUserGroups function added getUserGroupInfo

--- application/models/user_group_model.php
+++ application/models/user_group_model.php
@@ -13,5 +13,42 @@ class User_group_model extends MY_model{
 		return $this->mongo_db->count('user_group');
 	}
 
+	public function fetchAllUserGroups($data){
+		$this->set_site_mongodb(0);
+
+		if(isset($data['filter_name']) && !is_null($data['filter_name'])){
+			//Do something if filter name is set..
+		}
+
+		if(isset($data['start']) || isset($data['limit'])){
+			if ($data['start'] < 0) {
+                $data['start'] = 0;
+            }
+
+            if ($data['limit'] < 1) {
+                $data['limit'] = 20;
+            }
+
+            $this->mongo_db->limit((int)$data['limit']);
+            $this->mongo_db->offset((int)$data['start']);
+		}
+
+		$results = $this->mongo_db->get('user_group');
+
+		return $results;
+
+	}
+
+	public function getUserGroupInfo($user_group_id){
+		$this->mongo_db->where('_id', new MongoID($user_group_id));
+		$results = $this->mongo_db->get('user_group');
+
+		return ($results)? $results[0]:null;
+	}
+
+	public function getAllFeatures(){
+		return $this->mongo_db->get('playbasis_feature');
+	}
+
 
 }
