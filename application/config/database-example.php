f9133d4dd21ac3278408987aba1375c108db6071
vkado <wee@playbasis.com>
2014-01-21 12:36:37 +0700

    update config file

--- application/config/database-example.php
+++ application/config/database-example.php
@@ -81,55 +81,5 @@ $db['local']['swap_pre'] = '';
 $db['local']['autoinit'] = TRUE;
 $db['local']['stricton'] = FALSE;
 
-
-$db['developer']['hostname'] = 'dev.pbapp.net';
-$db['developer']['username'] = 'playbasis_admin';
-$db['developer']['password'] = 'devplaybasisdatabase';
-$db['developer']['database'] = 'core';
-$db['developer']['dbdriver'] = 'mysql';
-$db['developer']['dbprefix'] = '';
-$db['developer']['pconnect'] = TRUE;
-$db['developer']['db_debug'] = TRUE;
-$db['developer']['cache_on'] = FALSE;
-$db['developer']['cachedir'] = '';
-$db['developer']['char_set'] = 'utf8';
-$db['developer']['dbcollat'] = 'utf8_general_ci';
-$db['developer']['swap_pre'] = '';
-$db['developer']['autoinit'] = TRUE;
-$db['developer']['stricton'] = FALSE;
-
-
-$db['production']['hostname'] = 'db.pbapp.net';
-$db['production']['username'] = 'playbasis_admin';
-$db['production']['password'] = 'databaseplaybasisproduction';
-$db['production']['database'] = 'core';
-$db['production']['dbdriver'] = 'mysql';
-$db['production']['dbprefix'] = '';
-$db['production']['pconnect'] = TRUE;
-$db['production']['db_debug'] = TRUE;
-$db['production']['cache_on'] = FALSE;
-$db['production']['cachedir'] = '';
-$db['production']['char_set'] = 'utf8';
-$db['production']['dbcollat'] = 'utf8_general_ci';
-$db['production']['swap_pre'] = '';
-$db['production']['autoinit'] = TRUE;
-$db['production']['stricton'] = FALSE;
-
-$db['demo']['hostname'] = 'db.pbapp.net';
-$db['demo']['username'] = 'playbasis_admin';
-$db['demo']['password'] = 'databaseplaybasisproduction';
-$db['demo']['database'] = 'core_demo';
-$db['demo']['dbdriver'] = 'mysql';
-$db['demo']['dbprefix'] = '';
-$db['demo']['pconnect'] = TRUE;
-$db['demo']['db_debug'] = TRUE;
-$db['demo']['cache_on'] = FALSE;
-$db['demo']['cachedir'] = '';
-$db['demo']['char_set'] = 'utf8';
-$db['demo']['dbcollat'] = 'utf8_general_ci';
-$db['demo']['swap_pre'] = '';
-$db['demo']['autoinit'] = TRUE;
-$db['demo']['stricton'] = FALSE;
-
 /* End of file database.php */
 /* Location: ./application/config/database.php */
