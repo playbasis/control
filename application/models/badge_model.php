45ef027326042ff5616b5f21052ee304cc620c2e
vkado <wee@playbasis.com>
2014-01-22 18:27:49 +0700

    fix badge

--- application/models/badge_model.php
+++ application/models/badge_model.php
@@ -13,17 +13,26 @@ class Badge_model extends MY_Model
 	{
 		//get badge ids
 		$this->set_site_mongodb($data['site_id']);
-		$this->mongo_db->select(array('badge_id'));
+		$this->mongo_db->select(array('badge_id','image','name','description','hint'));
 		$this->mongo_db->where(array(
 			'client_id' => $data['client_id'],
-			'site_id' => $data['site_id']
+			'site_id' => $data['site_id'],
+            'deleted' => false
 		));
-		$badgeSet = $this->mongo_db->get('playbasis_badge_to_client');
-		if(!$badgeSet)
-			return array();
+        $badgeSet = $this->mongo_db->get('playbasis_badge_to_client');
+        $badges = array();
+        if(!$badgeSet)
+            return array();
+        foreach($badgeSet as $badge)
+        {
+            unset($badge['_id']);
+            array_push($badges, $badge);
+        }
+		//if(!$badges)
+		//	return array();
 		//get data on badges
-		$badges = array();
-		foreach($badgeSet as $badge)
+		//$badges = array();
+		/*foreach($badgeSet as $badge)
 		{
 			//get badge data
 			$this->mongo_db->select(array(
@@ -44,21 +53,25 @@ class Badge_model extends MY_Model
 			$badge['badge_id'] = $badge['_id'];
 			unset($badge['_id']);
 			array_push($badges, $badge);
-		}
+		}*/
 		return $badges;
 	}
 	public function getBadge($data)
 	{
 		//get badge id
 		$this->set_site_mongodb($data['site_id']);
-		$this->mongo_db->select(array('badge_id'));
-		$this->mongo_db->where(array(
-			'client_id' => $data['client_id'],
-			'site_id' => $data['site_id'],
-			'badge_id' => $data['badge_id']
-		));
+        $this->mongo_db->select(array('badge_id','image','name','description','hint'));
+        $this->mongo_db->where(array(
+            'client_id' => $data['client_id'],
+            'site_id' => $data['site_id'],
+            'badge_id' => $data['badge_id'],
+            'deleted' => false
+        ));
 		$result = $this->mongo_db->get('playbasis_badge_to_client');
-		if(!$result || !$result[0])
+        if(!$result)
+            return array();
+        unset($result[0]['_id']);
+		/*if(!$result || !$result[0])
 			return array();
 		$result = $result[0];
 		unset($result['_id']);
@@ -79,8 +92,8 @@ class Badge_model extends MY_Model
 		$result = $result[0];
 		$result['image'] = $this->config->item('IMG_PATH') . $result['image'];
 		$result['badge_id'] = $result['_id'];
-		unset($result['_id']);
-		return $result;
+		unset($result['_id']);*/
+		return $result ? $result[0] : array();
 	}
 }
 ?>
