381f7b55ea33ddd9718c9292f0119e54b3838d8b
vkado <wee@playbasis.com>
2014-01-22 16:28:17 +0700

    update exp

--- application/models/level_model.php
+++ application/models/level_model.php
@@ -18,30 +18,32 @@ class Level_model extends MY_Model
         $level_range = array($level, intval($level)+1);
 
         //check if client have their own exp table setup
-        $this->mongo_db->select("level,exp AS min_exp,level_title,image");
+        $this->mongo_db->select(array("level","exp","level_title","image"));
         $this->mongo_db->where('client_id', $client_id);
         $this->mongo_db->where('site_id', $site_id);
         $this->mongo_db->where_in("level", $level_range);
-        $this->mongo_db->where("status", '1');
-        $this->mongo_db->order_by('exp');
+        $this->mongo_db->where("status", true);
+        $this->mongo_db->order_by(array('exp' => 1));
         $this->mongo_db->limit(2);
         $levela = $this->mongo_db->get('playbasis_client_exp_table');
 
         if(empty($levela))
         {
             //get level from default exp table instead
-            $this->mongo_db->select("level,exp AS min_exp,level_title,image");
+            $this->mongo_db->select(array("level","exp","level_title","image"));
             $this->mongo_db->where_in("level", $level_range);
-            $this->mongo_db->where("status", '1');
-            $this->mongo_db->order_by('exp');
+            $this->mongo_db->where("status", true);
+            $this->mongo_db->order_by(array('exp' => 1));
             $this->mongo_db->limit(2);
             $levela = $this->mongo_db->get('playbasis_exp_table');
 
         }
-
         $i = 0;
         foreach($levela as $l){
-            $l['max_exp'] = (isset($levela[$i+1]))?intval($levela[$i+1]['min_exp'])-1:null;
+            $l['min_exp'] = $l['exp'];
+//            var_Dump($levela[$i+1]);
+            $l['max_exp'] = (isset($levela[$i+1]))?intval($levela[$i+1]['exp'])-1:null;
+            unset($l['exp']);
             $leveldata = $l;
             break;
         }
@@ -53,19 +55,19 @@ class Level_model extends MY_Model
     {
         $this->set_site_mongodb($site_id);
         //check if client have their own exp table setup
-        $this->mongo_db->select("level,exp AS min_exp,level_title,image");
+        $this->mongo_db->select(array("level","exp","level_title","image"));
         $this->mongo_db->where('client_id', $client_id);
         $this->mongo_db->where('site_id', $site_id);
-        $this->mongo_db->where("status", '1');
-        $this->mongo_db->order_by('exp');
+        $this->mongo_db->where("status", true);
+        $this->mongo_db->order_by(array('exp' => 1));
         $leveldata = $this->mongo_db->get('playbasis_client_exp_table');
 
         if(empty($leveldata))
         {
             //get level from default exp table instead
-            $this->mongo_db->select("level,exp AS min_exp,level_title,image");
-            $this->mongo_db->where("status", '1');
-            $this->mongo_db->order_by('exp');
+            $this->mongo_db->select(array("level","exp","level_title","image"));
+            $this->mongo_db->where("status", true);
+            $this->mongo_db->order_by(array('exp' => 1));
             $leveldata = $this->mongo_db->get('playbasis_exp_table');
 
         }
