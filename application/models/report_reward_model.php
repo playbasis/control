d9cff80b1ac4e7086a5d9783b68b12683c4fe0a2
Joe Prasetyo <joe@playbasis.com>
2014-02-05 15:28:30 +0700

    quick fix

--- application/models/report_reward_model.php
+++ application/models/report_reward_model.php
@@ -28,8 +28,8 @@ class Report_reward_model extends MY_Model{
         $this->mongo_db->where('client_id',  new MongoID($data['client_id']));
         $this->mongo_db->where('site_id',  new MongoID($data['site_id']));
 
-        if (isset($data['date_start']) && $data['date_start'] != '' && isset($data['date_modified']) && $data['date_modified'] != '' ) {
-            $this->mongo_db->where('date_added', array('$gt' => new MongoDate(strtotime($data['date_start'])), '$lte' => new MongoDate(strtotime($data['date_modified']))));
+        if (isset($data['date_start']) && $data['date_start'] != '' && isset($data['date_expire']) && $data['date_expire'] != '' ) {
+            $this->mongo_db->where('date_added', array('$gt' => new MongoDate(strtotime($data['date_start'])), '$lte' => new MongoDate(strtotime($data['date_expire']))));
         }
 
         if (isset($data['action_id']) && $data['action_id'] != ''){
@@ -66,8 +66,8 @@ class Report_reward_model extends MY_Model{
         $this->mongo_db->where('client_id',  new MongoID($data['client_id']));
         $this->mongo_db->where('site_id',  new MongoID($data['site_id']));
 
-        if (isset($data['date_start']) && $data['date_start'] != '' && isset($data['date_modified']) && $data['date_modified'] != '' ) {
-            $this->mongo_db->where('date_added', array('$gt' => new MongoDate(strtotime($data['date_start'])), '$lte' => new MongoDate(strtotime($data['date_modified']))));
+        if (isset($data['date_start']) && $data['date_start'] != '' && isset($data['date_expire']) && $data['date_expire'] != '' ) {
+            $this->mongo_db->where('date_added', array('$gt' => new MongoDate(strtotime($data['date_start'])), '$lte' => new MongoDate(strtotime($data['date_expire']))));
         }
 
         if (isset($data['action_id']) && $data['action_id'] != ''){
@@ -99,7 +99,7 @@ class Report_reward_model extends MY_Model{
     public function getRewardName($reward_id){
     	$this->mongo_db->where('_id', new MongoID($reward_id));
     	$var = $this->mongo_db->get('playbasis_reward');
-    	return $var[0];
+    	return isset($var[0])?$var[0]:null;
     }
 
     public function getRewardsBadgesSite($data){
