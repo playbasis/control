9dacbb215974bc8e03515f48e1292a3c032a119b
Joe Prasetyo <joe@playbasis.com>
2014-02-06 17:04:12 +0700

    excel helper

--- application/helpers/php-excel_helper.php
+++ application/helpers/php-excel_helper.php
@@ -5,9 +5,34 @@
  * 
  * This is one of my utility-classes.
  * 
+ * The MIT License
+ * 
+ * Copyright (c) 2007 Oliver Schwarz
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
  * @package Utilities
  * @author Oliver Schwarz <oliver.schwarz@gmail.com>
- * @version 0.2
+ * @version 1.0
  */
 
 /**
@@ -19,7 +44,7 @@
  * @package Utilities
  * @subpackage Excel
  * @author Oliver Schwarz <oliver.schwarz@vaicon.de>
- * @version 0.1
+ * @version 1.0
  *
   * @todo Add error handling (array corruption etc.)
  * @todo Write a wrapper method to do everything on-the-fly
