7f154412dcbcfb7735f55916285b939e812aae30
thanakij <pechpras@playbasis.com>
2014-04-03 19:20:46 +0700

    Initial new dashboard (insight) module

--- application/config/playbasis.php
+++ application/config/playbasis.php
@@ -1,5 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['IMG_PATH'] = 'https://images.pbapp.net/';
+$config['server'] = 'http://localhost/api/';
+//$config['server'] = 'http://api-carlos.pbapp.net/';
 
 ?>
