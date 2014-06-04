9334bdd7458a8636f99d62a90146e2462793bffc
vkado <wee_weerapat@hotmail.com>
2014-06-04 12:32:54 +0700

    clean code

--- application/controllers/goods.php
+++ application/controllers/goods.php
@@ -801,24 +801,6 @@ class Goods extends MY_Controller
         $this->output->set_output(json_encode($json));
     }
 
-    private function vsort (&$array, $key, $order='asc') {
-        $res=array();
-        $sort=array();
-        reset($array);
-        foreach ($array as $ii => $va) {
-            $sort[$ii]=$va[$key];
-        }
-        if(strtolower($order) == 'asc'){
-            asort($sort);
-        }else{
-            arsort($sort);
-        }
-        foreach ($sort as $ii => $va) {
-            $res[$ii]=$array[$ii];
-        }
-        $array=$res;
-    }
-
     public function checkGoodsIsPublic($goods_id){
         $allGoodsFromClients = $this->Goods_model->checkGoodsIsPublic($goods_id);
 
