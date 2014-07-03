87c608c91be47892718cf932d31324400f5ac3d5
Wee <wee_weerapat@hotmail.com>
2014-07-04 04:58:20 +0700

    fix player null

--- application/models/service_model.php
+++ application/models/service_model.php
@@ -27,7 +27,7 @@ class Service_model extends MY_Model
         $this->mongo_db->order_by(array('date_added' => -1));
         $event_log = $this->mongo_db->get('playbasis_event_log');
 
-        foreach($event_log as &$event){
+        foreach($event_log as $key => &$event){
 
             $this->mongo_db->where('site_id', $site_id);
             $this->mongo_db->where('_id', $event['pb_player_id']);
@@ -45,7 +45,7 @@ class Service_model extends MY_Model
 
             $event['player'] = isset($player[0]) ? $player[0] : null;
             if(!$event['player']){
-                unset($event);
+                unset($event_log[$key]);
                 continue;
             }
 
