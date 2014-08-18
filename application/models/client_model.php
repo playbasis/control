1045948cf703853419c8724f8733879bab35a240
vkado <wee_weerapat@hotmail.com>
2014-08-18 14:06:04 +0700

    fix action getaction gone

--- application/models/client_model.php
+++ application/models/client_model.php
@@ -21,6 +21,23 @@ class Client_model extends MY_Model
 		$this->mongo_db->where($clientData);
 		return $this->mongo_db->get('playbasis_rule');
 	}
+    public function getAction($clientData)
+    {
+        assert($clientData);
+        assert(is_array($clientData));
+        assert(isset($clientData['client_id']));
+        assert(isset($clientData['site_id']));
+        assert(isset($clientData['action_name']));
+        $this->set_site_mongodb($clientData['site_id']);
+        $this->mongo_db->select(array('action_id'));
+        $this->mongo_db->where(array(
+            'client_id' => $clientData['client_id'],
+            'site_id' => $clientData['site_id'],
+            'name' => $clientData['action_name']
+        ));
+        $id = $this->mongo_db->get('playbasis_action_to_client');
+        return ($id && $id[0]) ? $id[0] : array();
+    }
 	public function getActionId($clientData)
 	{
 		assert($clientData);
