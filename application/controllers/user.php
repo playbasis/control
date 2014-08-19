97b1fb907567b4815ed54451ec9fbcf581a6d1cc
Joe Prasetyo <joe@playbasis.com>
2014-08-19 14:31:30 +0700

    Submit and check if the image does not exists
    Before if the image does not exists, it will save as the current default image  (the camera default image)

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -860,9 +860,11 @@ class User extends MY_Controller
                     'password'=>$this->input->post('password'),
                     'confirm_password' =>$this->input->post('password_confirm'),
                     'edit_account'=>true,
-                    'image' =>$this->input->post('image'),
                 );
-                if($this->form_validation->run()){
+                if($this->input->post('image') != ""){
+                    $data['image'] =$this->input->post('image');
+                }
+                if($this->input->post('password') != '' && $this->input->post('image') != '' && $this->form_validation->run()){
                     $this->User_model->editUser($user_id, $data);
                 }
             }
