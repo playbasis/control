56d1b529fa0b3e21884249d29e8875106dae8484
thanakij <pechpras@playbasis.com>
2014-09-04 18:10:38 +0700

    Add constant

--- application/config/constants.php
+++ application/config/constants.php
@@ -64,6 +64,7 @@ define('EMAIL_TYPE_NOTIFY_INACTIVE_CLIENTS', 'notifyInactiveClients');
 define('EMAIL_TYPE_NOTIFY_FREE_ACTIVE_CLIENTS', 'notifyFreeActiveClientsToSubscribe');
 define('EMAIL_TYPE_NOTIFY_NEAR_LIMIT_USAGE', 'notifyNearLimitUsage');
 define('EMAIL_TYPE_REMIND_TO_SETUP_SUBSCRIPTION', 'remindClientsToSetupSubscription');
+define('EMAIL_TYPE_REMIND_END_OF_TRIAL_PERIOD', 'remindClientsEndOfTrialPeriod');
 
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
