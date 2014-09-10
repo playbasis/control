2e07ea12a12736241feeeff6a36c680cc28be524
vkado <wee_weerapat@hotmail.com>
2014-09-10 17:42:09 +0700

    fix

--- application/models/client_model.php
+++ application/models/client_model.php
@@ -208,16 +208,21 @@ class Client_model extends MY_Model
     public function addPlanToPermission($data){
         $this->set_site_mongodb($this->session->userdata('site_id'));
 
-        $this->mongo_db->where('site_id', new MongoID($data['site_id']));
-        $this->mongo_db->delete('playbasis_permission');
+        if($data['site_id']){
+            $this->mongo_db->where('site_id', new MongoID($data['site_id']));
+            $this->mongo_db->delete('playbasis_permission');
+        }
 
         $data_insert = array(
             'plan_id' =>  new MongoID($data['plan_id']),
             'client_id' =>  new MongoID($data['client_id']),
-            'site_id' => new MongoID($data['site_id']),
+            'site_id' =>  null,
             'date_added' => new MongoDate(strtotime(date("Y-m-d H:i:s"))),
             'date_modified' => new MongoDate(strtotime(date("Y-m-d H:i:s"))),
         );
+        if($data['site_id']){
+            $data_insert['site_id'] = new MongoID($data['site_id']);
+        }
 
         return $this->mongo_db->insert('playbasis_permission', $data_insert);
     }
@@ -481,7 +486,7 @@ class Client_model extends MY_Model
         $this->set_site_mongodb($this->session->userdata('site_id'));
 
         $this->mongo_db->where('client_id', new MongoID($client_id));
-        $this->mongo_db->delete('playbasis_permission');
+        $this->mongo_db->delete_all('playbasis_permission');
     }
 
     public function getSitesByClientId($client_id){
--- application/models/permission_model.php
+++ application/models/permission_model.php
@@ -17,16 +17,21 @@ class Permission_model extends MY_Model
     public function addPlanToPermission($data){
         $this->set_site_mongodb($this->session->userdata('site_id'));
 
-        $this->mongo_db->where('site_id', new MongoID($data['site_id']));
-        $this->mongo_db->delete('playbasis_permission');
+        if($data['site_id']){
+            $this->mongo_db->where('site_id', new MongoID($data['site_id']));
+            $this->mongo_db->delete('playbasis_permission');
+        }
 
         $data_insert = array(
             'plan_id' =>  new MongoID($data['plan_id']),
             'client_id' =>  new MongoID($data['client_id']),
-            'site_id' => new MongoID($data['site_id']),
+            'site_id' =>  null,
             'date_added' => new MongoDate(strtotime(date("Y-m-d H:i:s"))),
             'date_modified' => new MongoDate(strtotime(date("Y-m-d H:i:s"))),
         );
+        if($data['site_id']){
+            $data_insert['site_id'] = new MongoID($data['site_id']);
+        }
 
         return $this->mongo_db->insert('playbasis_permission', $data_insert);
     }
