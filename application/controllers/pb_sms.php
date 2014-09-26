e97ec2bac1c29ae2ef0094669bab7e6708f44a24
vkado <wee_weerapat@hotmail.com>
2014-09-26 18:26:31 +0700

    fix

--- application/controllers/pb_sms.php
+++ application/controllers/pb_sms.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 require_once APPPATH . '/libraries/REST2_Controller.php';
-class Sms extends REST2_Controller
+class Pb_sms extends REST2_Controller
 {
     public function __construct()
     {
