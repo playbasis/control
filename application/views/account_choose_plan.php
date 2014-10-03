d98bffe944edf284278257d021a7bf07c4e25dd7
notjiam <notjiam@gmail.com>
2014-10-03 15:52:25 +0700

    Style Regis

--- application/views/account_choose_plan.php
+++ application/views/account_choose_plan.php
@@ -1,16 +1,16 @@
-<div id="content" class="span10">
-    <div class="box">
-        <div class="heading">
-            <h1><img src="<?php echo base_url();?>image/category.png" alt="" /> <?php echo $heading_title; ?></h1>
-            <div class="buttons">
-                <button class="btn btn-info" onclick="location = baseUrlPath+'account/add_site'" type="button"><?php echo $this->lang->line('button_back'); ?></button>
-                <button class="btn btn-info" onclick="$('#form').submit();" type="button"><?php echo $this->lang->line('button_next'); ?></button>
-            </div><!-- .buttons -->
-        </div><!-- .heading -->
-        <div class="content">
-            <?php if($this->session->flashdata('fail')){ ?>
+<div id="content" class="regis-page-wrapper">
+
+
+<div class="row regis-header">
+    <h1>
+            <small>Get started with Gamification today!</small>
+            Choose plan
+    </h1>
+</div>
+    
+    <?php if($this->session->flashdata('fail')){ ?>
                 <div class="content messages half-width">
-                <div class="warning"><?php echo $this->session->flashdata('fail'); ?></div>
+                    <div class="warning"><?php echo $this->session->flashdata('fail'); ?></div>
                 </div>
             <?php }?>
             <?php if(validation_errors() || isset($message)){?>
@@ -21,31 +21,149 @@
                     <?php }?>
                 </div>
             <?php }?>
+
             <?php $attributes = array('id' => 'form');?>
             <?php echo form_open($form ,$attributes); ?>
-                <div id="tab-general">
-                    <table class="form">
-                        <tr>
-                            <td><span class="required">*</span> <?php echo $this->lang->line('entry_plan'); ?>:</td>
-                            <td>
-                                <select name="plan_id">
-                                    <option value=""><?php echo $this->lang->line('text_select'); ?></option>
-                                    <?php if ($plan_data) { ?>
-                                        <?php foreach ($plan_data as $each) { ?>
-                                            <?php if ($plan_id==$each['_id']) { ?>
-                                                <option value="<?php echo $each['_id']; ?>" selected="selected"><?php echo $each['name']; ?></option>
-                                            <?php } else { ?>
-                                                <option value="<?php echo $each['_id']; ?>"><?php echo $each['name']; ?></option>
-                                            <?php } ?>
-                                        <?php } ?>
-                                    <?php } ?>
-                                </select>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
+                                
+                                <input type="hidden" name="plan_id" >
+                                
+                            
             <?php echo form_close(); ?>
-        </div><!-- .content -->
-        1 [2] 3
-    </div><!-- .box -->
+        
+        <!-- Start plans -->
+          <div class="plans-wrapper">
+              <section id="pricePlans">
+
+              <div class="pricePlansPro">
+                
+                Improve the level of your customers' engagement by getting all the gamification features your business needs with our Playbasis gamification platform
+                
+              </div>
+
+              
+              <ul id="plans">
+                
+              <?php
+                $n = 0;
+               foreach ($plan_data as $key => $plan) {
+                  
+                  if($plan['price'] == 0){
+                    ?>
+                    <li class="plan plan-try plan-free plan-left">
+                      <ul class="planContainer">
+                        <li class="title">
+                          
+                            <h2>Free</h2>
+                         
+                        </li>
+                        <li class="price"><p><?php echo $plan['name'] ?></p></li>
+                        <li>
+                          <ul class="options">
+                            <li><?php echo $plan['limit_others']['player'] ?> Register Users</li>
+                            <li><?php echo $plan['limit_others']['user'] ?> Admin Users</li>
+                            <li><?php echo !empty( $plan['limit_requests']['/player'] ) ? $plan['limit_requests']['/player'].' Player Req API/Sec.' : '-' ?> </li>
+                            <li><?php echo !empty( $plan['limit_requests']['/engine'] ) ? $plan['limit_requests']['/engine'].'Engine Req API/Sec.' : '-' ?> </li>
+                          </ul>
+                        </li>
+                        <li class="button"><a  href="javascript:void(0)" class="plan-btn" data-plan-id="<?php echo $plan['_id'] ?>">Try it Free</a></li>
+                      </ul>
+                    </li>
+
+                    <?php
+                  }else{
+                        $n++;
+                        ?>
+                        
+                        <li class="plan plan-pro ">
+                          <ul class="planContainer">
+                            <li class="title"><h2>$<?php echo $plan['price'] ?><span>/month</span></h2></li>
+                            <li class="price"><p><?php echo $plan['name'] ?></p></li>
+                            <li>
+                              <ul class="options">
+                                <li><?php echo $plan['limit_others']['player'] ?> Register Users</li>
+                                <li><?php echo $plan['limit_others']['user'] ?> Admin Users</li>
+                                <li><?php echo !empty( $plan['limit_requests']['/player'] ) ? $plan['limit_requests']['/player'].' Player Req API/Sec.' : '-' ?> </li>
+                            <li><?php echo !empty( $plan['limit_requests']['/engine'] ) ? $plan['limit_requests']['/engine'].'Engine Req API/Sec.' : '-' ?> </li>
+                              </ul>
+                            </li>
+                            <li class="button"><a href="javascript:void(0)" class="plan-btn" data-plan-id="<?php echo $plan['_id'] ?>">Start</a></li>
+                          </ul>
+                        </li>
+
+                        <?php
+
+                  }
+
+              } ?>
+
+              <li class="plan plan-enterprise plan-right">
+                    <ul class="planContainer">
+                      <li class="title"><h2>Call us</h2></li>
+                      <li class="price"><p>ENTERPRISE</p></li>
+                      <li>
+                        <ul class="options">
+                          <li>Unlimit Register Users</li>
+                        <li>Unlimit Admin Users</li>
+                        <li>Unlimit Player Req API/Sec.</li>
+                        <li>Unlimit Engine Req API/Sec.</li>
+                        </ul>
+                      </li>
+                      <li class="button"><a href="mailto:info@playbasis.com">Call us</a></li>
+                    </ul>
+                  </li>
+                
+              </ul> <!-- End ul#plans -->
+            </section>
+          </div>
+          <!-- End plans -->
+
+          
+
+<div class="regis-content span8 offset2">
+    </div><!-- .content -->
+
+    <div class="clearfix"></div>
+    <div class="regis-step-wrapper text-center span6 offset3">
+        
+        <div class="row">
+            <div class="span4 " >
+                <div class="step-wrapper">
+                    <h6>Step 1</h6>
+                    <div class="step-icon"><i class="fa fa-globe"></i></div>
+                    <h4>Add a<br> Site Name</h4>
+                </div>
+            </div>
+            <div class="span4" >
+                <div class="step-wrapper  current-step">
+                    <h6>Step 2</h6>
+                    <div class="step-icon"><i class="fa fa-check-square-o"></i></div>
+                    <h4>Choose plan</h4>
+                </div>
+            </div>
+            <div class="span4" >
+                <div class="step-wrapper">
+                    <h6>Step 3</h6>
+                    <div class="step-icon"><i class="fa fa-trophy"></i></div>
+                    <h4>Let's Start</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+     <div class="clearfix"></div>
+
 </div><!-- #content .span10 -->
+
+
+
+
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        
+        $('.plan-btn').click(function(){
+            $('input[name=plan_id]').val($(this).attr('data-plan-id'));
+            $('form#form').submit();
+        })
+
+    });
+</script>
--- application/views/account_start.php
+++ application/views/account_start.php
@@ -1,37 +1,83 @@
-<div id="content" class="span10">
-    <div class="box">
-        <div class="heading">
-            <h1><img src="<?php echo base_url();?>image/category.png" alt="" /> <?php echo $heading_title; ?></h1>
-            <div class="buttons">
-                <button class="btn btn-info" onclick="location = baseUrlPath+'account/choose_plan'" type="button"><?php echo $this->lang->line('button_back'); ?></button>
-                <button class="btn btn-info" onclick="$('#form').submit();" type="button"><?php echo $this->lang->line('button_done'); ?></button>
-            </div><!-- .buttons -->
-        </div><!-- .heading -->
-        <div class="content">
-            <?php if($this->session->flashdata('fail')){ ?>
-                <div class="content messages half-width">
-                <div class="warning"><?php echo $this->session->flashdata('fail'); ?></div>
+<div id="content" class="regis-page-wrapper">
+
+
+<div class="row regis-header">
+    <h1>
+            <small>Get started with Gamification today!</small>
+            <?php echo $heading_title; ?>
+    </h1>
+</div>
+    <div class="regis-content span8 offset2">
+        <?php if($this->session->flashdata('fail')){ ?>
+            <div class="content messages half-width">
+            <div class="warning"><?php echo $this->session->flashdata('fail'); ?></div>
+            </div>
+        <?php }?>
+        <?php if(validation_errors() || isset($message)){?>
+            <div class="content messages half-width">
+                <?php echo validation_errors('<div class="warning">', '</div>');?>
+                <?php if (isset($message) && $message){?>
+                    <div class="warning"><?php echo $message;?></div>
+                <?php }?>
+            </div>
+        <?php }?>
+        <?php $attributes = array('id' => 'form', 'class' => 'pbf-form text-center');?>
+        <?php echo form_open($form ,$attributes); ?>
+            
+            <fieldset>
+
+              
+                <div class="row ">
+                    <p>
+                    You are just one step away for finalizing your account. <br>Please click the button above to proceed.
+                    </p>
+                    <ul class="span4 offset4">
+                        <li><i class="fa fa-check-circle-o fa-lg"></i> 5000 Register Users</li>
+                        <li><i class="fa fa-check-circle-o fa-lg"></i> 2 Admin Users</li>
+                        <li><i class="fa fa-check-circle-o fa-lg"></i> 10000 Player Req API/Sec. </li>
+                        <li><i class="fa fa-check-circle-o fa-lg"></i> 100000Engine Req API/Sec.</li>
+                    </ul>
                 </div>
-            <?php }?>
-            <?php if(validation_errors() || isset($message)){?>
-                <div class="content messages half-width">
-                    <?php echo validation_errors('<div class="warning">', '</div>');?>
-                    <?php if (isset($message) && $message){?>
-                        <div class="warning"><?php echo $message;?></div>
-                    <?php }?>
+
+
+
+              <hr>
+
+              <button class="btn btn-primary" type="submit">Start</button>
+
+            </fieldset>
+            
+
+        <?php echo form_close(); ?>
+    </div><!-- .content -->
+
+    <div class="clearfix"></div>
+    <div class="regis-step-wrapper text-center span6 offset3">
+        
+        <div class="row">
+            <div class="span4 " >
+                <div class="step-wrapper">
+                    <h6>Step 1</h6>
+                    <div class="step-icon"><i class="fa fa-globe"></i></div>
+                    <h4>Add a<br> Site Name</h4>
                 </div>
-            <?php }?>
-            <?php $attributes = array('id' => 'form');?>
-            <?php echo form_open($form ,$attributes); ?>
-                <div id="tab-general">
-                    <table class="form">
-                        <tr>
-                            <td>You are just one step away for finalizing your account. Please click the button above to proceed.</td>
-                        </tr>
-                    </table>
+            </div>
+            <div class="span4" >
+                <div class="step-wrapper">
+                    <h6>Step 2</h6>
+                    <div class="step-icon"><i class="fa fa-check-square-o"></i></div>
+                    <h4>Choose plan</h4>
                 </div>
-            <?php echo form_close(); ?>
-        </div><!-- .content -->
-        1 2 [3]
-    </div><!-- .box -->
+            </div>
+            <div class="span4" >
+                <div class="step-wrapper  current-step">
+                    <h6>Step 3</h6>
+                    <div class="step-icon"><i class="fa fa-trophy"></i></div>
+                    <h4>Let's Start</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+     <div class="clearfix"></div>
+
 </div><!-- #content .span10 -->
