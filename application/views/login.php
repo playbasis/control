c84a8a473e09fb84fb187a5e09b6293a1624e746
notjiam <notjiam@gmail.com>
2014-10-07 20:13:25 +0700

    Renew Login page

--- application/views/login.php
+++ application/views/login.php
@@ -1,10 +1,14 @@
-<div id="content">
-    <div class="box" style="width: 400px; min-height: 300px; margin-top: 40px; margin-left: auto; margin-right: auto;">
-        <div class="heading">
-            <h1><img src="<?php echo base_url();?>image/lockscreen.png" alt="" /> <?php echo $this->lang->line('text_login'); ?></h1>
-        </div>
-        <div class="content" style="min-height: 150px; overflow: hidden;">
-            <?php
+<div id="content" class="regis-page-wrapper regis-login-page">
+
+
+<div class="row regis-header">
+    <h1>
+            <small>Get started with Gamification today!</small>
+            Login
+    </h1>
+</div>
+    <div class="regis-content span6 offset3">
+       <?php
             if(validation_errors() || isset($message)) {
                 ?>
                 <div class="content messages half-width">
@@ -20,10 +24,10 @@
                 </div>
                 <?php
             }
-            $udata = array('name' => 'username', 'id' => 'username','value' => set_value('username'), 'class'=>'btn-not-login');
-            $pdata = array('name' => 'password', 'id' => 'password', 'class'=>'btn-not-login');
+            $udata = array('name' => 'username', 'id' => 'username','value' => set_value('username'), 'class'=>'btn-not-login', 'placeholder'=>'E-mail');
+            $pdata = array('name' => 'password', 'id' => 'password', 'class'=>'btn-not-login', 'placeholder'=>'Password');
 
-            $attributes = array('id' => 'form');
+            $attributes = array('id' => 'form', 'class' => 'pbf-form');
             echo form_open('login',$attributes);
             ?>
             <?php if($this->session->flashdata('email_sent')){ ?>
@@ -36,39 +40,43 @@
                 <div class="warning"><?php echo $this->session->flashdata('multi_login'); ?></div>
                 </div>
             <?php }?>
-                <table style="width: 100%;">
-                    <tr>
-                        <td style="text-align: center;" rowspan="4"><img src="<?php echo base_url();?>image/login.png" alt="<?php echo $this->lang->line('text_login'); ?>" /></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $this->lang->line('entry_username'); ?>:<br />
-                            <?php echo form_input($udata); ?>
-                            <br />
-                            <br />
-                            <?php echo $this->lang->line('entry_password'); ?>:<br />
-                            <?php echo form_password($pdata); ?>
-                            <br/>
-                            <?php echo anchor('forgot_password', $this->lang->line('text_forgot_password'), array('class' => 'btn-not-login')); ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td style="text-align: right;">
-                            <a onclick="$('#form').submit();" class="button btn-not-login"><?php echo $this->lang->line('button_login'); ?></a>
-                        </td>
-                    </tr>
-                </table>
-                <?php if (isset($redirect)) { ?>
-                <input type="hidden" name="redirect" value="<?php echo $redirect; ?>" />
-                <?php } ?>
-            <?php
-            echo form_close();
-            ?>
-        </div>
-    </div>
-</div>
+
+            
+            <fieldset>
+
+              
+                <div class="input-center">
+                    <!-- <input type="text" name="site" value="<?php echo isset($site) ? $site : set_value('site'); ?>" size="10" placeholder="<?php echo $this->lang->line('entry_site'); ?>" /> -->
+                    <?php echo form_input($udata); ?>
+                    <?php echo form_password($pdata); ?><br>
+                    <?php echo anchor('forgot_password', $this->lang->line('text_forgot_password'), array('class' => 'btn-not-login')); ?>
+                </div>
+
+
+
+              <hr>
+
+              <!-- <button class="btn btn-primary" type="submit"><?php echo $this->lang->line('button_next'); ?></button> -->
+              <button onclick="$('#form').submit();" type="submit" class=" btn-not-login"><?php echo $this->lang->line('button_login'); ?></button>
+
+            </fieldset>
+            
+
+        <?php echo form_close(); ?>
+    </div><!-- .content -->
+
+    <div class="clearfix"></div>
+  
+
+</div><!-- #content .span10 -->
+
+
+
+
+
+
+
+
 <script type="text/javascript"><!--
 $('#form input').keydown(function(e) {
     if (e.keyCode == 13) {
