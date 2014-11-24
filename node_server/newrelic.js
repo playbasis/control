474c9445e5d571494b938986246924ccd8884736
vkado <wee_weerapat@hotmail.com>
2014-11-24 15:00:59 +0700

    try to ignore socket io

--- node_server/newrelic.js
+++ node_server/newrelic.js
@@ -5,20 +5,25 @@
  * description of configuration variables and their potential values.
  */
 exports.config = {
-  /**
-   * Array of application names.
-   */
-  app_name : ['Node Server'],
-  /**
-   * Your New Relic license key.
-   */
-  license_key : 'bce66656873bd8019b770cd1901a615f8496e573',
-  logging : {
     /**
-     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
-     * issues with the agent, 'info' and higher will impose the least overhead on
-     * production applications.
-     */
-    level : 'info'
-  }
+    * Array of application names.
+    */
+    app_name : ['Node Server'],
+    /**
+    * Your New Relic license key.
+    */
+    license_key : 'bce66656873bd8019b770cd1901a615f8496e573',
+    logging : {
+        /**
+         * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+         * issues with the agent, 'info' and higher will impose the least overhead on
+         * production applications.
+         */
+        level : 'info'
+    },
+    rules : {
+        ignore : [
+            '^/socket.io/.*/xhr-polling'
+        ]
+    }
 };
