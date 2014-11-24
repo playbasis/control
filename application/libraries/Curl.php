1271c7985b3be6c84bfb90840a7cb8c71533735a
thanakij <pechpras@playbasis.com>
2014-11-24 17:25:22 +0700

    Change Curl default connection timeout and operation timeout for insights module

--- application/libraries/Curl.php
+++ application/libraries/Curl.php
@@ -290,7 +290,11 @@ class Curl {
 // Set two default options, and merge any extra ones in
         if ( ! isset($this->options[CURLOPT_TIMEOUT]))
         {
-            $this->options[CURLOPT_TIMEOUT] = 30;
+            $this->options[CURLOPT_TIMEOUT] = 60;
+        }
+        if ( ! isset($this->options[CURLOPT_CONNECTTIMEOUT]))
+        {
+            $this->options[CURLOPT_CONNECTTIMEOUT] = 30;
         }
         if ( ! isset($this->options[CURLOPT_RETURNTRANSFER]))
         {
