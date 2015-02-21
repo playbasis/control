6a7cf99030875501c8d2376ec65b51091233e1db
thanakij <pechpras@playbasis.com>
2015-02-22 00:34:19 +0700

    Introduce "getTemplateByTemplateId" for sending email/sms with templates

--- application/models/email_model.php
+++ application/models/email_model.php
@@ -112,5 +112,15 @@ class Email_model extends MY_Model
 		$results = $this->mongo_db->get('playbasis_email_to_client');
 		return $results ? $results[0] : null;
 	}
+
+	public function getTemplateByTemplateId($site_id, $template_id) {
+		$this->set_site_mongodb($site_id);
+		$this->mongo_db->where('name', $template_id);
+		$this->mongo_db->where('status', true);
+		$this->mongo_db->where('deleted', false);
+		$this->mongo_db->limit(1);
+		$results = $this->mongo_db->get('playbasis_email_to_client');
+		return $results ? $results[0] : null;
+	}
 }
 ?>
