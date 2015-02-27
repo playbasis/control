ef4f9db60f64413feb9cbd0861fecb9a61086172
thanakij <pechpras@playbasis.com>
2015-02-27 18:17:18 +0700

    Remove unused file

--- application/config/playbasis.php
+++
@@ -1,8 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config['IMG_PATH'] = 'https://images.pbapp.net/';
-//$config['server'] = 'http://localhost/api/';
-//$config['server'] = 'http://api-carlos.pbapp.net/';
-$config['server'] = 'https://api.pbapp.net/';
-
-?>
