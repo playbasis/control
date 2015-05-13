044936e0e035ff1f4e6aceeca433d42b74bc801c
thanakij <pechpras@playbasis.com>
2015-05-13 17:15:02 +0700

    - Make use of constant in email sending - Add firstname, lastname

--- application/controllers/cron.php
+++ application/controllers/cron.php
@@ -354,7 +354,7 @@ $email = 'pechpras@playbasis.com';
 			$from = EMAIL_FROM;
 			$to = $email;
 			$subject = '[Playbasis] Please Verify Your Phone Number';
-			$html = $this->parser->parse('message_verify_mobile.html', array('firstname' => $client['first_name'], 'lastname' => $client['last_name']), true);
+			$html = $this->parser->parse('message_verify_mobile.html', array('firstname' => $client['first_name'], 'lastname' => $client['last_name'], 'date' => date('M d, Y', strtotime(DATE_FREE_ACCOUNT_SHOULD_SETUP_MOBILE))), true);
 			$response = $this->utility->email($from, array($to, EMAIL_BCC_PLAYBASIS_EMAIL), $subject, $html, null);
 			$this->email_model->log(EMAIL_TYPE_CLIENT_REGISTRATION, $client_id, null, $response, $from, $to, $subject, $html);
 		}
