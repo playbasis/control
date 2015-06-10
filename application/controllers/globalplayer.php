b743d47d99ced5d3cae385779891e92f9d3849b7
Patsita Kongkaew <patsita.kongkaew@gmail.com>
2015-06-10 15:03:08 +0700

    edit header on global player controller

--- application/controllers/globalplayer.php
+++ application/controllers/globalplayer.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 require_once APPPATH . '/libraries/REST2_Controller.php';
-require_once APPPATH . '/libraries/ApnsPHP/Autoload.php';
+//require_once APPPATH . '/libraries/ApnsPHP/Autoload.php';
 class GlobalPlayer extends REST2_Controller
 {
 	public function __construct()
