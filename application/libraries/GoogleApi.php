36ec6b974bc8e4d46c39d34a466a182851478896
thanakij <pechpras@playbasis.com>
2015-06-15 16:43:24 +0700

    Modify Google API wrapper to support a flow on dashboard

--- application/libraries/GoogleApi.php
+++ application/libraries/GoogleApi.php
@@ -2,11 +2,9 @@
 
 require_once ("Google/autoload.php");
 
-define('APPLICATION_NAME', 'Google Calendar API');
-define('CREDENTIALS_PATH', 'application/config/google-credentials.json');
-define('CLIENT_SECRET_PATH', 'application/config/google-client_secret.json');
+define('APPLICATION_NAME', 'Playbasis Dashboard');
 define('SCOPES', implode(' ', array(
-    Google_Service_Calendar::CALENDAR_READONLY)
+    Google_Service_Calendar::CALENDAR)
 ));
 
 class GoogleApi
@@ -19,62 +17,45 @@ class GoogleApi
     {
         $this->_ci =& get_instance();
         $this->_config = $config;
-        $this->_client = $this->getClient();
+    }
+
+    public function initialize($clientId, $clientSecret, $redirectUri=null) {
+        $this->_client = $this->getClient($clientId, $clientSecret, $redirectUri);
+        return $this;
     }
 
     /**
      * Returns an authorized API client.
      * @return Google_Client the authorized client object
      */
-    private function getClient() {
+    private function getClient($clientId, $clientSecret, $redirectUri=null) {
         $client = new Google_Client();
         $client->setApplicationName(APPLICATION_NAME);
         $client->setScopes(SCOPES);
-        $client->setAuthConfigFile(CLIENT_SECRET_PATH);
+        $client->setClientId($clientId);
+        $client->setClientSecret($clientSecret);
+        if ($redirectUri) $client->setRedirectUri($redirectUri);
         $client->setAccessType('offline');
-
-        // Load previously authorized credentials from a file.
-        $credentialsPath = $this->expandHomeDirectory(CREDENTIALS_PATH);
-        if (file_exists($credentialsPath)) {
-            $accessToken = file_get_contents($credentialsPath);
-        } else {
-            // Request authorization from the user.
-            $authUrl = $client->createAuthUrl();
-            printf("Open the following link in your browser:\n%s\n", $authUrl);
-            print 'Enter verification code: ';
-            $authCode = trim(fgets(STDIN));
-
-            // Exchange authorization code for an access token.
-            $accessToken = $client->authenticate($authCode);
-
-            // Store the credentials to disk.
-            if(!file_exists(dirname($credentialsPath))) {
-                mkdir(dirname($credentialsPath), 0700, true);
-            }
-            file_put_contents($credentialsPath, $accessToken);
-            printf("Credentials saved to %s\n", $credentialsPath);
-        }
-        $client->setAccessToken($accessToken);
-
-        // Refresh the token if it's expired.
-        if ($client->isAccessTokenExpired()) {
-            $client->refreshToken($client->getRefreshToken());
-            file_put_contents($credentialsPath, $client->getAccessToken());
-        }
         return $client;
     }
 
-    /**
-     * Expands the home directory alias '~' to the full path.
-     * @param string $path the path to expand.
-     * @return string the expanded path.
-     */
-    private function expandHomeDirectory($path) {
-        $homeDirectory = getenv('HOME');
-        if (empty($homeDirectory)) {
-            $homeDirectory = getenv("HOMEDRIVE") . getenv("HOMEPATH");
+    public function setAccessToken($accessToken) {
+        $this->_client->setAccessToken(is_string($accessToken) ? $accessToken : json_encode($accessToken));
+        // Refresh the token if it's expired.
+        if ($this->_client->isAccessTokenExpired()) {
+            $this->_client->refreshToken($this->_client->getRefreshToken());
         }
-        return str_replace('~', realpath($homeDirectory), $path);
+        return $this;
+    }
+
+    public function createAuthUrl() {
+        return $this->_client->createAuthUrl();
+    }
+
+    public function authenticate($authCode) {
+        $accessToken = $this->_client->authenticate($authCode);
+        $this->setAccessToken($accessToken);
+        return json_decode($accessToken);
     }
 
     public function calendar() {
