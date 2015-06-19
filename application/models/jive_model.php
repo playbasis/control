041bef31b06bdf1ee14635a74a125cb198a1256b
thanakij <pechpras@playbasis.com>
2015-06-19 21:10:25 +0700

    Remove unused hasToken in Jive

--- application/models/jive_model.php
+++ application/models/jive_model.php
@@ -60,14 +60,6 @@ class Jive_model extends MY_Model {
         $this->mongo_db->update("playbasis_jive_to_client");
     }
 
-    public function hasToken($site_id) {
-        $this->set_site_mongodb($this->session->userdata('site_id'));
-        $this->mongo_db->where('site_id', new MongoID($site_id));
-        $this->mongo_db->where_exists('token');
-        $this->mongo_db->where_ne('deleted', true);
-        return $this->mongo_db->count("playbasis_jive_to_client") > 0;
-    }
-
     public function listEvents($site_id, $per_page, $offset) {
         return array_slice($this->events, $offset, $per_page);
     }
