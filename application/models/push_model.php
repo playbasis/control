78763d43464f0ff5e976bf4419761bef97195d5d
thanakij <pechpras@playbasis.com>
2015-07-01 19:46:52 +0700

    Add missing lines

--- application/models/push_model.php
+++ application/models/push_model.php
@@ -1,11 +1,12 @@
 <?php
-
+defined('BASEPATH') OR exit('No direct script access allowed');
 date_default_timezone_set('Asia/Bangkok');
 class Push_model extends MY_Model
 {
     public function __construct()
     {
         parent::__construct();
+        $this->load->library('mongo_db');
     }
 
     public function initial($data,$type=null)
