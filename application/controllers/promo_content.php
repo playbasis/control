433c313e85358ad7a64aa2951e51ac3864eb99dc
Burachit-PlayBasis <burachit.t@playbasis.com>
2015-09-03 23:10:04 +0700

    Initial add placeholder stuff

---
+++ application/controllers/promo_content.php
@@ -0,0 +1,144 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH . '/libraries/MY_Controller.php';
+
+
+class Promo_content extends MY_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('User_model');
+        if(!$this->User_model->isLogged()){
+            redirect('/login', 'refresh');
+        }
+
+        $lang = get_lang($this->session, $this->config);
+        $this->lang->load($lang['name'], $lang['folder']);
+        $this->lang->load("promo_content", $lang['folder']);
+    }
+
+    public function index(){
+
+        if(!$this->validateAccess()){
+            echo "<script>alert('".$this->lang->line('error_access')."'); history.go(-1);</script>";
+            die();
+        }
+
+        $this->data['meta_description'] = $this->lang->line('meta_description');
+        $this->data['title'] = $this->lang->line('title');
+        $this->data['heading_title'] = $this->lang->line('heading_title');
+
+        $this->getList(0);
+    }
+
+    public function page($offset=0) {
+
+        if(!$this->validateAccess()){
+            echo "<script>alert('".$this->lang->line('error_access')."'); history.go(-1);</script>";
+            die();
+        }
+
+        $this->data['meta_description'] = $this->lang->line('meta_description');
+        $this->data['title'] = $this->lang->line('title');
+        $this->data['heading_title'] = $this->lang->line('heading_title');
+
+        $this->getList($offset);
+    }
+
+    public function getList($offset){
+        $client_id = $this->User_model->getClientId();
+        $site_id = $this->User_model->getSiteId();
+
+        $this->load->library('pagination');
+
+        $config['per_page'] = NUMBER_OF_RECORDS_PER_PAGE;
+
+        $filter = array(
+            'limit' => $config['per_page'],
+            'start' => $offset,
+            'client_id'=>$client_id,
+            'site_id'=>$site_id,
+            'sort'=>'sort_order'
+        );
+
+        if(isset($_GET['filter_name'])){
+            $filter['filter_name'] = $_GET['filter_name'];
+        }
+
+        $config['base_url'] = site_url('promo_content/page');
+        $config["uri_segment"] = 3;
+        $config['total_rows'] = 0;
+        
+        if($client_id){
+            // todo: insert data query here to show in dashboard list
+        }
+
+        $config['num_links'] = NUMBER_OF_ADJACENT_PAGES;
+
+        $config['next_link'] = 'Next';
+        $config['next_tag_open'] = "<li class='page_index_nav next'>";
+        $config['next_tag_close'] = "</li>";
+
+        $config['prev_link'] = 'Prev';
+        $config['prev_tag_open'] = "<li class='page_index_nav prev'>";
+        $config['prev_tag_close'] = "</li>";
+
+        $config['num_tag_open'] = '<li class="page_index_number">';
+        $config['num_tag_close'] = '</li>';
+
+        $config['cur_tag_open'] = '<li class="page_index_number active"><a>';
+        $config['cur_tag_close'] = '</a></li>';
+
+        $config['first_link'] = 'First';
+        $config['first_tag_open'] = '<li class="page_index_nav next">';
+        $config['first_tag_close'] = '</li>';
+
+        $config['last_link'] = 'Last';
+        $config['last_tag_open'] = '<li class="page_index_nav prev">';
+        $config['last_tag_close'] = '</li>';
+
+        $this->pagination->initialize($config);
+
+        $this->data['pagination_links'] = $this->pagination->create_links();
+        $this->data['pagination_total_pages'] = ceil(floatval($config["total_rows"]) / $config["per_page"]);
+        $this->data['pagination_total_rows'] = $config["total_rows"];
+
+        $this->data['main'] = 'promo_content';
+        $this->render_page('template');
+    }
+
+    private function validateModify() {
+        if ($this->User_model->hasPermission('modify', 'promo_content')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private function validateAccess(){
+        if($this->User_model->isAdmin()){
+            return true;
+        }
+        $this->load->model('Feature_model');
+        $client_id = $this->User_model->getClientId();
+
+        if ($this->User_model->hasPermission('access', 'promo_content') &&  $this->Feature_model->getFeatureExistByClientId($client_id, 'promo_content')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+function index_badge_id($obj) {
+    return $obj['badge_id'];
+}
+
+function index_reward_id($obj) {
+    return $obj['reward_id'];
+}
+?>
---
+++ application/language/english/promo_content_lang.php
@@ -0,0 +1,11 @@
+<?php
+// Heading
+$lang['heading_title']          		= 'Promotional Content';
+
+$lang['button_filter']							= 'Filter';
+$lang['button_clear_filter']					= 'Clear filter';
+
+$lang['error_permission']       = 'Warning: You do not have permission to modify Promotional Content!';
+$lang['error_access']       	= 'Warning: You do not have permission to access this page!';
+$lang['error_promo_contents_limit']       = 'You have reached Promotional Content limit.';
+?>
