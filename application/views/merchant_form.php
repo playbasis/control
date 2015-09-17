dd49d35c9b827be052234295342949ce9d4bb580
Burachit-PlayBasis <burachit.t@playbasis.com>
2015-09-17 20:11:50 +0700

    Still working to optimize js dynamic input creation

--- application/views/merchant_form.php
+++ application/views/merchant_form.php
@@ -15,7 +15,23 @@
                 <div class="content messages half-width">
                     <div class="warning"><?php echo $this->session->flashdata('limit_reached'); ?></div>
                 </div>
-            <?php } ?>
+            <?php }
+            if (validation_errors() || isset($message)) {
+            ?>
+            <div class="content messages half-width">
+                <?php
+                echo validation_errors('<div class="warning">', '</div>');
+
+                if (isset($message) && $message) {
+                    ?>
+                    <div class="warning"><?php echo $message; ?></div>
+                    <?php
+                }
+                ?>
+            </div>
+            <?php
+            }
+            ?>
             <div class="tabbable">
                 <ul class="nav nav-tabs" id="merchantTab">
                     <li class="active"><a href="#merchant-general"
@@ -25,26 +41,11 @@
                     </li>
                 </ul>
                 <?php
-                if (validation_errors() || isset($message)) {
-                    ?>
-                    <div class="content messages half-width">
-                        <?php
-                        echo validation_errors('<div class="warning">', '</div>');
-
-                        if (isset($message) && $message) {
-                            ?>
-                            <div class="warning"><?php echo $message; ?></div>
-                            <?php
-                        }
-                        ?>
-                    </div>
-                    <?php
-                }
                 $attributes = array('id' => 'form', 'class' => 'form-horizontal');
                 echo form_open($form, $attributes);
                 ?>
                 <div class="tab-content">
-                    <div class="tab-pane active" id="merchant-general">
+                    <div class="tab-pane fade active in" id="merchant-general">
                         <div class="container-fluid">
                             <div class="row-fluid">
                                 <div class="control-group">
@@ -79,38 +80,64 @@
                         </div>
                     </div>
                     <!-- .tab-pane -->
-                    <div class="tab-pane" id="merchant-branch">
+                    <div class="tab-pane fade" id="merchant-branch">
                         <div class="container-fluid">
-                            <div class="row-fluid">
-                                <div class="text-center form-inline">
-                                    <label for="merchant-branch-to-create"><?php echo $this->lang->line('entry_number_to_create'); ?></label>
-                                    <input type="text" class="input-small" id="merchant-branch-to-create">
-                                    <button type="submit" class="btn">Create</button>
+                            <div class="tabbable">
+                                <ul class="nav nav-tabs">
+                                    <li class="active"><a href="#branches-list">Branches list</a></li>
+                                    <li><a href="#branches-new">New Branches</a></li>
+                                </ul>
+                                <div class="tab-content">
+                                    <div class="tab-pane fade active in" id="branches-list">
+                                        <div class="row-fluid">
+                                            <table data-toggle="table" data-pagination="true">
+                                                <thead>
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th>Branch</th>
+                                                    <th>PIN Code</th>
+                                                    <th>Status</th>
+                                                </tr>
+                                                </thead>
+                                                <tbody>
+                                                <tr>
+                                                    <td colspan="4" style="text-align: center">No branch found. Create new?</td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                    <div class="tab-pane fade" id="branches-new">
+                                        <div class="row-fluid">
+                                            <div class="text-center form-inline">
+                                                <label
+                                                    for="merchant-branch-to-create"><?php echo $this->lang->line('entry_number_to_create'); ?></label>
+                                                <input type="text" class="input-small" id="merchant-branch-to-create">
+                                                <button type="submit" class="btn">Create</button>
+                                            </div>
+                                        </div>
+                                        <div class="row-fluid">
+                                            <a id="add">+</a>
+                                            <table class="table" id="new-branches-table">
+                                                <thead>
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th>Branch</th>
+                                                    <th>Status</th>
+                                                </tr>
+                                                </thead>
+                                                <tbody>
+                                                <tr>
+                                                    <td id="branch-index">1</td>
+                                                    <td><input type="text" name="branches[0][branchName]"></td>
+                                                    <td><input type="checkbox" name="branches[0][status]"
+                                                               data-handle-width="40" checked="checked"></td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row-fluid">
-                                <table class="table table-bordered">
-                                    <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Branch</th>
-                                        <th>PIN Code</th>
-                                        <th>Status</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-<!--                                    <tr>-->
-<!--                                        <td>###</td>-->
-<!--                                        <td><input type="text" class="input-small" id="merchant-branch-name" name="merchant-branch-name[]"></td>-->
-<!--                                        <td>#code</td>-->
-<!--                                        <td><input type="checkbox" name="merchant-branch-status[]" id="branch_status_switch"-->
-<!--                                                   data-handle-width="40" checked="checked"></td>-->
-<!--                                    </tr>-->
-                                    <tr>
-                                        <td colspan="4" style="text-align: center">Create new branch?</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
                             </div>
                         </div>
                     </div>
@@ -124,15 +151,39 @@
     </div>
 </div>
 <link href="<?php echo base_url(); ?>stylesheet/custom/bootstrap-switch.min.css" rel="stylesheet" type="text/css">
+<link href="<?php echo base_url(); ?>stylesheet/custom/bootstrap-table.min.css" rel="stylesheet" type="text/css">
 <script src="<?php echo base_url(); ?>javascript/custom/bootstrap-switch.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>javascript/custom/bootstrap-table.min.js" type="text/javascript"></script>
 <script type="text/javascript">
+    var globalNewIndex = 0;
+
     $(function () {
-        $('#merchantTab a').click(function (e) {
+        $("#add").click(function () {
+            $("[name^='branches'][name$='[status]']").bootstrapSwitch("destroy");
+            $('#new-branches-table tbody>tr:last').clone(true).insertAfter('#new-branches-table tbody>tr:last');
+
+            var newIndex = globalNewIndex + 1;
+            var changeIds = function (i, val) {
+                if(val)
+                    return val.replace(globalNewIndex, newIndex);
+            };
+
+            $('#new-branches-table tbody>tr:last input').attr('name', changeIds).attr('id', changeIds);
+
+            var displayIndex = $('#new-branches-table tbody>tr:last #branch-index').text(); //display index start from 1
+            $('#new-branches-table tbody>tr:last #branch-index').html(parseInt(displayIndex)+1);
+
+            globalNewIndex++;
+            $("[name^='branches']").filter("[name$='[status]']").bootstrapSwitch();
+            return false;
+        });
+
+        $("ul.nav-tabs a").click(function (e) {
             e.preventDefault();
             $(this).tab('show');
         });
 
         $("[name='merchant-status']").bootstrapSwitch();
-        $("#status_switch").bootstrapSwitch();
+        $("[name^='branches']").filter("[name$='[status]']").bootstrapSwitch();
     });
 </script>
