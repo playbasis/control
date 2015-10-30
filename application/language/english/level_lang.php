2b9359871d96eb4e9e35500d45bfd26305bbe7a5
thanakij <pechpras@playbasis.com>
2015-10-30 14:08:52 +0700

    Update description text and image for default text when there is no level configured

--- application/language/english/level_lang.php
+++ application/language/english/level_lang.php
@@ -10,9 +10,10 @@ $lang['text_all']				= 'All';
 $lang['text_success']			= 'You have successfully created a level';
 $lang['text_success_update']	= 'You have successfully updated a level';
 $lang['text_success_delete']	= 'You have successfully deleted level(s)';
-$lang['text_guide_level']		= 'Create levels automatically using one of provided templates. Just specify how many levels you need and select template.<br> This is how your levels progression will look like:<br>
+$lang['text_guide_level']		= 'You can add levels automatically using one of provided templates: Curve, Pokemon, Disgea. Just choose how many levels you need and select template.<br>
+Level progression for each template (for first 20 levels):<br>
 [img]<br>
-Or add levels manually specifying your own values.';
+You can also add levels manually.';
 
 // Button
 $lang['button_add']						= 'Add Badge';
