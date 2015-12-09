78ddb3bd28f2fb4915d9f537a8cf6aa4cbe293de
notjiam <notjiam@gmail.com>
2015-12-09 15:10:51 +0700

    Add front-end on boarding survey

--- application/views/partial/landingpage_partial.php
+++ application/views/partial/landingpage_partial.php
@@ -63,8 +63,190 @@
     </div>
 </div>
 
+
+ 
+<!-- Modal -->
+<div id="modal_survey" class="modal hide fade" tabindex="-1" role="dialog" >
+  <div class="modal-header">
+      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+      <h3>Give us a sec…</h3>
+  </div>
+  <div class="modal-body">
+      <form id="survey_form">
+            <div class="step step-1">
+                  <div class="form-group">
+                        <h4>What is your business sector?</h4>
+                        <select id="business_sector" name="business_sector">
+                              <option value="">---------- Select one ----------</option>
+                              <option value="Agencies">Agencies</option>
+                              <option value="Banking/Financial">Banking/Financial</option>
+                              <option value="Call Centers">Call Centers</option>
+                              <option value="Consumer Brands">Consumer Brands</option>
+                              <option value="E-Commerce">E-Commerce</option>
+                              <option value="Education">Education</option>
+                              <option value="Enterprise">Enterprise</option>
+                              <option value="Food and Beverage">Food and Beverage</option>
+                              <option value="Publishers/Media">Publishers/Media</option>
+                              <option value="Retail/Wholesale">Retail/Wholesale</option>
+                              <option value="Sales/CRM">Sales/CRM</option>
+                              <option value="Telecommunications">Telecommunications</option>
+                              <option value="Others">Others</option>
+                        </select>
+                  </div>
+                  <div class="modal-action">
+                        <a href="javascript:void(0)" class="btn btn-primary next-btn" >Next</a>
+                  </div>
+            </div>
+            <div class="step step-2">
+                  <div class="form-group form-feature-wrapper">
+                        <h4>Which features you are looking for?</h4>
+                        <div class="clearfix">
+                              <div class="span6">
+                                    <label><input type="checkbox" name="feature[api]" > API</label>
+                                    <label><input type="checkbox" name="feature[sdk]" > SDK</label>
+                                    <label><input type="checkbox" name="feature[widgets]" > Widgets</label>
+                                    <label><input type="checkbox" name="feature[dashboardAnalytics]"> Dashboard Analytics</label>
+                                    <label><input type="checkbox" name="feature[referralProgram]" > Referral Program</label>
+                                    <label><input type="checkbox" name="feature[loyaltyProgram]" > Loyalty Program</label>
+                                    <label><input type="checkbox" name="feature[ruleEngine]" > Rule Engine</label>
+                              </div>
+                              <div class="span6">
+                                    <label><input type="checkbox" name="feature[socialMediaIntegration]"> Social Media Integration</label>
+                                    <label><input type="checkbox" name="feature[rewardStore]" > Reward Store</label>
+                                    <label><input type="checkbox" name="feature[whiteLabelPlatform]"> White Label Platform</label>
+                                    <label><input type="checkbox" name="feature[consultationService]"> Consultation Service</label>
+                                    <label><input type="checkbox" name="feature[leaderboardLeaderboard]" > Leaderboard</label>
+                                    <label><input type="checkbox" name="feature[transactionIntegration]" > Transaction Integration</label>
+                                    <label><input type="checkbox" name="feature[miniGames]" > Mini Games</label>
+                                    <label><input type="checkbox" name="feature[others]" > Others</label>
+                              </div>
+                        </div>
+                  </div>
+                  <div class="modal-action">
+                        <a href="javascript:void(0)" class="btn back-btn">Back</a>
+                        <a href="javascript:void(0)" class="btn btn-primary next-btn" >Next</a>
+                  </div>
+            </div>
+            <div class="step step-3">
+                 <div class="form-group form-objective-wrapper">
+                       <h4>What is the objective of your project?</h4>
+                       <div class="clearfix">
+                               <div class="span4 offset4">
+                                    <label><input type="checkbox" name="objective[motivateUsers]" > Motivate Users</label>
+                                    <label><input type="checkbox" name="objective[motivateEmployees]" > Motivate Employees</label>
+                                    <label><input type="checkbox" name="objective[increaseEngagement]" > Increase Engagement</label>
+                                    <label><input type="checkbox" name="objective[customerLoyalty]" > Customer Loyalty</label>
+                                    <label><input type="checkbox" name="objective[digitalTransformation]" > Digital Transformation</label>
+                                    <label><input type="checkbox" name="objective[others]" > Others</label>
+                              </div>
+                       </div>
+                 </div>
+                  <div class="modal-action">
+                        <a href="javascript:void(0)" class="btn back-btn">Back</a>
+                        <a href="javascript:void(0)" class="btn btn-primary next-btn" >Finish</a>
+                  </div>
+            </div>
+    </form>
+    
+  </div>
+</div>
+
 <script type="text/javascript">
 	$(document).ready(function(){
+
+                                    $('#modal_survey').modal({
+                                          backdrop: 'static',
+                                          show: true
+                                    });
+
+                                    $('.next-btn').click(function(){
+                                          if( $(this).hasClass('disabled')  ){
+                                                return;
+                                          }
+                                          $step = $(this).closest('.step');
+                                          
+                                          if( $step.hasClass('step-1') ){
+                                                if( $("#business_sector").val() == ""){
+                                                      alert('Please select business sector');
+                                                }else{
+                                                      $('.step').hide();
+                                                      $('.step-2').show();      
+                                                }
+                                                
+
+                                          }else if( $step.hasClass('step-2') ){
+
+                                                if ( $(".form-feature-wrapper input[type=checkbox]:checked").length <= 0 ){ 
+                                                      alert('Please select atleast one feature');
+                                                }else{
+                                                      $('.step').hide();
+                                                      $('.step-3').show();      
+                                                }
+                                                
+
+                                          }else if( $step.hasClass('step-3') ){
+                                              
+                                                if ( $(".form-objective-wrapper input[type=checkbox]:checked").length <= 0 ){ 
+                                                      alert('Please select atleast one objective');
+                                                }else{
+                                                      
+                                                      $("#survey_form").submit();
+                                                      $step.find('.btn').addClass('disabled');
+                                                }
+                                          }
+                                    });
+                                    
+
+                                    $("#survey_form").submit(function(e){
+                                          e.preventDefault();
+                                          var data = $( this ).serializeArray();
+                                          $.ajax({
+                                            type: "POST",
+                                            url: 'https://www.pbapp.net',
+                                            data: data,
+                                            success: function(data){
+                                                setTimeout(function(){
+                                                      $('.step .btn').removeClass('disabled');
+                                                      $('#modal_survey').modal('hide');     
+                                                },5000);
+                                            }
+                                          });
+                                    });
+
+                                    $('#modal_survey').on('hidden', function () {
+                                          $.ajax({
+                                            type: "POST",
+                                            url: 'https://www.pbapp.net',
+                                            data: {
+                                                dosurvey: true
+                                            },
+                                            success: function(data){
+                                                
+                                            }
+                                          });
+                                    });
+                                    
+
+                                    $('.back-btn').click(function(){
+                                          if( $(this).hasClass('disabled')  ){
+                                                return;
+                                          }
+                                          $step = $(this).closest('.step');
+                                          
+                                          if( $step.hasClass('step-1') ){
+
+
+                                          }else if( $step.hasClass('step-2') ){
+
+                                                $('.step').hide();
+                                                $('.step-1').show();
+
+                                          }else if( $step.hasClass('step-3') ){
+                                                $('.step').hide();
+                                                $('.step-2').show();
+                                          }
+                                    });
+
 		$('.free-plan-info').popover({
 			html: true,
 			container: 'body',
