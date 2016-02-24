d7a71b4a466c866ab8e7a434fd3bf50371187268
Niti Chaivanichaya <niti.c@playbasis.com>
2016-02-24 18:43:02 +0700

    Add_support_for_level_condition - Add dropdown list for level condition.

--- javascript/rule_editor/rule_dataset.js
+++ javascript/rule_editor/rule_dataset.js
@@ -536,6 +536,31 @@ DataSet = function(jsonArray, parent_id, json_jigsaw) {
                                 rowField.find('input').val(rowText.html());
                             }
                         }
+                        else if(anotherType.match('LEVEL')) {
+                            if($thisrow.find('.dropdown').length > 0) {
+                                console.log('LEVEL in if');
+                                var operation = {
+                                    'equal': '=',
+                                    'equalOrGreater': '>=',
+                                    'equalOrLess': '<=',
+                                    'greater': '>',
+                                    'less': '<'
+
+                                }
+                                var operation_option = $('<select id="operation">');
+                                $.each(operation, function (key, value) {
+                                    operation_option.append($('<option>', {value: key})
+                                        .text(value));
+                                });
+                                rowField.children().hide();
+                                rowField.append(operation_option);
+                                $('#operation').val(rowText.html());
+                                //console.log('CUSTOMPARAMETER');
+                            }else{
+                                if(DEBUG)console.log('edit > text');
+                                rowField.find('input').val(rowText.html());
+                            }
+                        }
                         else{
                             // default case
                             if(DEBUG)console.log('edit > normal case');
@@ -730,6 +755,23 @@ DataSet = function(jsonArray, parent_id, json_jigsaw) {
                                 rowField.find('input')
                                     .val(val)
                                 //console.log('save > CUSTOMPARAMETER' + val);
+                            }else {
+                                rowText.html(rowField.find('input').val());
+                            }
+                        }
+                        else if(anotherType.match('LEVEL')) {
+                            if($thisrow.find('.dropdown').length > 0) {
+                                var fulldate = $('.timepickerx').datetimepicker('getDate');
+                                var val_min = fulldate.getMinutes();
+                                var val_hours = fulldate.getHours();
+
+                                val_min = val_min < 10 ? '0' + val_min.toString() : val_min.toString();
+                                val_hours = val_hours < 10 ? '0' + val_hours.toString() : val_hours.toString();
+                                var val = val_hours + ':' + val_min;
+
+                                rowText.html(val);
+                                rowField.find('input')
+                                    .val(val)
                             }else {
                                 rowText.html(rowField.find('input').val());
                             }
