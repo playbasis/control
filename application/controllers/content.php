8e07430688433b279b1274f5d22bcb143174b955
Burachit-PlayBasis <burachit.t@playbasis.com>
2016-03-07 14:19:18 +0700

    Move `ApnsPHP_Log_Hidden` class to another file instead.

--- application/controllers/content.php
+++ application/controllers/content.php
@@ -779,16 +779,3 @@ class Content extends MY_Controller
     }
 
 }
-
-class ApnsPHP_Log_Hidden implements ApnsPHP_Log_Interface
-{
-    /**
-     * Logs a message.
-     *
-     * @param  $sMessage @type string The message.
-     */
-    public function log($sMessage)
-    {
-        return;
-    }
-}
---
+++ application/libraries/ApnsPHP/Log/Hidden.php
@@ -0,0 +1,14 @@
+<?php
+
+class ApnsPHP_Log_Hidden implements ApnsPHP_Log_Interface
+{
+    /**
+     * Logs a message.
+     *
+     * @param  $sMessage @type string The message.
+     */
+    public function log($sMessage)
+    {
+        return;
+    }
+}
