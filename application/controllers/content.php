8a91b1437970c120dc8ef0a2070d4c891df5c4de
Niti Chaivanichaya <niti.c@playbasis.com>
2016-06-02 16:58:14 +0700

    Bug during create content to add date

--- application/controllers/content.php
+++ application/controllers/content.php
@@ -368,8 +368,8 @@ class Content extends REST2_Controller
             $contentInfo['pb_player_id'] = $pb_player_id;
         }
         $contentInfo['image']      = ($this->input->post('image')) ? $this->input->post('image') : "no_image.jpg";
-        $contentInfo['date_start'] = (isset($contentInfo['date_start']) && !empty($contentInfo['date_start'])) ? new MongoDate(strtotime($this->input->post('date_start'))) : null;
-        $contentInfo['date_end']   = (isset($contentInfo['date_end']) && !empty($contentInfo['date_end'])) ? new MongoDate(strtotime($this->input->post('date_end'))) : null;
+        $contentInfo['date_start'] = !empty($this->input->post('date_start')) ? new MongoDate(strtotime($this->input->post('date_start'))) : null;
+        $contentInfo['date_end']   = !empty($this->input->post('date_end')) ? new MongoDate(strtotime($this->input->post('date_end'))) : null;
         $contentInfo['status']     = strtolower($this->input->post('status')) == 'true';
 
         if ($this->input->post('pin')){
