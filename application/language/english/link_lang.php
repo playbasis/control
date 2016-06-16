b0d0728386a8f63c069a517d2ad143f4cf1bb6a4
Pongsakorn Ruadsong <pongsakorn.r@playbasis.com>
2016-06-16 15:01:57 +0700

    - created new feature to configure link (support only branch.io)

---
+++ application/language/english/link_lang.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$lang['heading_title']          		= 'Link configuration';
+
+// Text
+$lang['text_success']       = 'Successfully updated configuration ';
+$lang['text_fail']          = 'Failed to update configuration, please try again later';
+
+// Button
+$lang['button_save']        = 'Save';
+$lang['button_cancel']      = 'Cancel';
+
+// Entry
+$lang['entry_type']         = 'Type';
+$lang['entry_key']          = 'Key';
+
+// Placeholder
+$lang['placeholder_branch_key']     = 'Insert branch key';
+
+// Error
+$lang['error_permission']   = 'Warning: You do not have permission to modify link configuration';
+$lang['error_access']  	    = 'Warning: You do not have permission to access this page';
+?>
