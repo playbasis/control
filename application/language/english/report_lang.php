3691de664a91026d58a6fef2f6e0507c649b59d7
Piya Phuakwilai <piya.p@playbasis.com>
2016-10-05 12:45:02 +0700

    filter goods status in report

--- application/language/english/report_lang.php
+++ application/language/english/report_lang.php
@@ -1,54 +1,55 @@
 <?php
 // Heading
-$lang['heading_title']          				= 'Report';
+$lang['heading_title']                          = 'Report';
 
 // Text
-$lang['text_success']           				= 'Success: You have modified clients!';
-$lang['text_default']           				= 'Default';
-$lang['text_image_manager']     				= 'Image Manager';
-$lang['text_browse']            				= 'Browse Files';
-$lang['text_clear']             				= 'Clear Image';
-$lang['text_criteria']          				= 'Domain View';
-$lang['text_confirm']			           		= 'Remove cannot be undone! Are you sure you want to do this?';
+$lang['text_success']                           = 'Success: You have modified clients!';
+$lang['text_default']                           = 'Default';
+$lang['text_image_manager']                     = 'Image Manager';
+$lang['text_browse']                            = 'Browse Files';
+$lang['text_clear']                             = 'Clear Image';
+$lang['text_criteria']                          = 'Domain View';
+$lang['text_confirm']                           = 'Remove cannot be undone! Are you sure you want to do this?';
 
 // Column
-$lang['column_avatar']							= 'Avatar';
-$lang['column_player_id']            			= 'ID';
-$lang['column_username']            			= 'Username';
-$lang['column_email']          				= 'Email';
-$lang['column_level']          				= 'Level';
-$lang['column_exp']          				    = 'Exp';
-$lang['column_action_name']          			= 'Action';
-$lang['column_url']      				        = 'URL OR FILTER STRING';
-$lang['column_date_added']          			= 'DateTime';
-$lang['column_reward_name']						= 'Reward';
-$lang['column_reward_value']					= 'Value';
-$lang['column_goods_amount']					= 'Amount';
-$lang['column_goods_status']					= 'Status';
-$lang['column_goods_redeemwith']					= 'Redeem with';
-$lang['column_goods_name']					= 'Goods';
-$lang['column_date_registered']					= 'Date Registered';
-$lang['column_quest_name']					    = 'Quest';
-$lang['column_mission_name']					= 'Mission Name';
-$lang['column_mission_number']					= 'Mission Number';
-$lang['column_quiz_name']						= 'Quiz';
-$lang['column_question_name']					= 'Question';
-$lang['column_option']							= 'Option';
-$lang['column_score']							= 'Score';
-$lang['column_max_score']						= 'Max Score';
+$lang['column_avatar']                          = 'Avatar';
+$lang['column_player_id']                       = 'ID';
+$lang['column_username']                        = 'Username';
+$lang['column_email']                           = 'Email';
+$lang['column_level']                           = 'Level';
+$lang['column_exp']                             = 'Exp';
+$lang['column_action_name']                     = 'Action';
+$lang['column_url']                             = 'URL OR FILTER STRING';
+$lang['column_date_added']                      = 'DateTime';
+$lang['column_reward_name']                     = 'Reward';
+$lang['column_reward_value']                    = 'Value';
+$lang['column_goods_amount']                    = 'Amount';
+$lang['column_goods_status']                    = 'Status';
+$lang['column_goods_redeemwith']                = 'Redeem with';
+$lang['column_goods_name']                      = 'Goods';
+$lang['column_date_registered']                 = 'Date Registered';
+$lang['column_quest_name']                      = 'Quest';
+$lang['column_mission_name']                    = 'Mission Name';
+$lang['column_mission_number']                  = 'Mission Number';
+$lang['column_quiz_name']                       = 'Quiz';
+$lang['column_question_name']                   = 'Question';
+$lang['column_option']                          = 'Option';
+$lang['column_score']                           = 'Score';
+$lang['column_max_score']                       = 'Max Score';
 
 // Button
-$lang['button_filter']							= 'Filter';
-$lang['button_download']						= 'Download';
-$lang['filter_date_start']						= 'Date Start';
-$lang['filter_date_end']						= 'Date End';
-$lang['filter_username']						= 'Username';
-$lang['filter_email_username']						= 'Email / Username';
-$lang['filter_action_id']						= 'Action';
-$lang['filter_reward_id']						= 'Reward';
-$lang['filter_site_id']							= 'Site';
+$lang['button_filter']                          = 'Filter';
+$lang['button_download']                        = 'Download';
+$lang['filter_date_start']                      = 'Date Start';
+$lang['filter_date_end']                        = 'Date End';
+$lang['filter_username']                        = 'Username';
+$lang['filter_email_username']                  = 'Email / Username';
+$lang['filter_action_id']                       = 'Action';
+$lang['filter_reward_id']                       = 'Reward';
+$lang['filter_status']                          = 'Status';
+$lang['filter_site_id']                         = 'Site';
 
 //Error messages
-$lang['error_permission']       	= 'Warning: You do not have permission to modify rules!';
-$lang['error_access']				= 'Warning: You do not have permission to access this page!'
+$lang['error_permission']                       = 'Warning: You do not have permission to modify rules!';
+$lang['error_access']                           = 'Warning: You do not have permission to access this page!';
 ?>
