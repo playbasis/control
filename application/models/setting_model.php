0ad45cc42b8fe8d168a342e85a63ad207950a1ab
Piya Phuakwilai <piya.p@playbasis.com>
2016-10-14 13:58:26 +0700

    fix app setting status

--- application/models/setting_model.php
+++ application/models/setting_model.php
@@ -29,8 +29,9 @@ class Setting_model extends MY_Model
 
         $this->mongo_db->where('client_id', new MongoId($client_id));
         $this->mongo_db->where('site_id', new MongoId($site_id));
-        $this->mongo_db->where(array('$or' => array(array('app_enable' => true) , array('app_enable' => array('exists' => false)))));
-        $this->mongo_db->where(array('$or' => array(array('app_period' => null) , array('app_period' => array('exists' => false)) , array('$and' => array(array('app_period.date_start' => array('$lt' => new MongoDate())), array('app_period.date_end' => array('$gt' => new MongoDate())))))));
+        $this->mongo_db->where(array('$and' => array(array('$or' => array(array('app_status' => true) , array('app_status' => array('$exists' => false)))),
+                                                     array('$or' => array(array('app_period' => null) , array('$and' => array(array('app_period.date_start' => array('$lt' => new MongoDate())),
+                                                                                                                              array('app_period.date_end' => array('$gt' => new MongoDate())))))))));
         $results = $this->mongo_db->get("playbasis_setting");
 
         return $results ? true : false;
