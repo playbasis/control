b63e859112f8edff1c00fb7d7aa4a5d850bba580
Piya Phuakwilai <piya.p@playbasis.com>
2016-12-06 13:24:00 +0700

    fix campaign active for null

--- application/models/campaign_model.php
+++ application/models/campaign_model.php
@@ -40,9 +40,9 @@ class Campaign_model extends MY_Model
             'site_id' => $site_id,
             'deleted' => false
         ));
-        $this->mongo_db->where_lte('date_start', $d);
-        $this->mongo_db->where_gte('date_end', $d);
-        $this->mongo_db->order_by(array('weight' => 'ASC', 'name' => 'ASC'));
+        $this->mongo_db->where(array('$and' => array( array('$or' => array(array("date_start" => null), array("date_start" => array('$lte'=> $d)))),
+                                                      array('$or' => array(array("date_end" => array('$gte'=> $d)), array("date_end" => null))))));
+        $this->mongo_db->order_by(array('weight' => 'desc','date_start' => 'desc', "date_end" => 'asc' , 'name' => 'asc'));
         $result = $this->mongo_db->get('playbasis_campaign_to_client');
 
         return $result ? $result[0]:array();
