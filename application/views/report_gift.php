fa8ffa5841d55b566ef16905f3dde582c00bee51
Piya Phuakwilai <piya.p@playbasis.com>
2017-09-26 17:33:32 +0700

    gift report

--- application/views/report_gift.php
+++ application/views/report_gift.php
@@ -6,10 +6,10 @@
         <div class="content">
         <div id="tabs" class="htabs">
             <a href="<?php echo site_url('report/action');?>" style="display:inline;">Actions</a>
-            <a href="<?php echo site_url('report/rewards_badges');?>" class="selected" style="display:inline;">Badges</a>
+            <a href="<?php echo site_url('report/rewards_badges');?>" style="display:inline;">Rewards</a>
             <a href="<?php echo site_url('report/rewards_custompoint');?>" style="display:inline;">Custompoints</a>
             <a href="<?php echo site_url('report/goods');?>" style="display:inline;">Goods</a>
-            <a href="<?php echo site_url('report/gift');?>" style="display:inline;">Gift</a>
+            <a href="<?php echo site_url('report/gift');?>" class="selected" style="display:inline;">Gift</a>
             <a href="<?php echo site_url('report/registration');?>" style="display:inline;">Registration</a>
             <a href="<?php echo site_url('report/quest');?>" style="display:inline;">Quest</a>
             <a href="<?php echo site_url('report/quiz');?>" style="display:inline;">Quiz</a>
@@ -17,16 +17,16 @@
             <div class="report-filter">
                 <div>
                 <span>
-                        <?php echo $this->lang->line('filter_date_start'); ?>
+                    <?php echo $this->lang->line('filter_date_start'); ?>
                     <input type="text" name="filter_date_start" value="<?php echo $filter_date_start; ?>" id="date-start" size="12" style="width:150px;"/>
                 </span>
                 <span>
-                        <?php echo $this->lang->line('filter_date_end'); ?>
+                    <?php echo $this->lang->line('filter_date_end'); ?>
                     <input type="text" name="filter_date_end" value="<?php echo $filter_date_end; ?>" id="date-end" size="12" style="width:150px;"/>
                 </span>
                 <span>
                     <?php echo $this->lang->line('filter_time_zone'); ?>
-                    <select id="filter_timezone" name="filter_timezone" style="height: 30px">
+                    <select id="filter_timezone" name="filter_timezone" style="height: 30px;">
                         <option value="0">Select timezone</option>
                         <?php foreach($time_zone as $t) {
                             if ($filter_time_zone == $t) { ?>
@@ -38,8 +38,17 @@
                     </select>
                 </span>
                 <span>
-                        <?php echo $this->lang->line('filter_email_username'); ?>
+                    <?php echo $this->lang->line('filter_email_username'); ?>
                     <input type="text" name="filter_username" value="<?php echo $filter_username; ?>" id="username" size="12" />
+                </span>
+                <span>
+                    <?php echo $this->lang->line('filter_status'); ?>
+                        <select class="chosen-select" id="filter_gift_status" name="filter_gift_status" style="height: 30px; width:80px">
+                        <option <?php if($filter_status == "all") echo "selected"; ?> value="all"><?php echo "All";    ?></option>
+                        <option <?php if($filter_status == "GOODS") echo "selected"; ?> value="GOODS"><?php echo "GOODS"; ?></option>
+                        <option <?php if($filter_status == "BADGE") echo "selected"; ?> value="BADGE"><?php echo "BADGE";   ?></option>
+                        <option <?php if($filter_status == "CUSTOM_POINT") echo "selected"; ?> value="CUSTOM_POINT"><?php echo "CUSTOM_POINT";    ?></option>
+                    </select>
                 </span>
                 <span>
                     <a onclick="filter();" class="button"><?php echo $this->lang->line('button_filter'); ?></a>
@@ -48,65 +57,36 @@
                     <a onclick="downloadFile();return true;" class="button"><?php echo $this->lang->line('button_download'); ?></a>
                 </span>
                 </div>
-                <span>
-                    <?php echo $this->lang->line('filter_badge_id'); ?>
-                    <select id="filter_badge_id" multiple name="filter_badge_id" style="width:90%">
-                    <?php foreach ($badge_rewards as $br){
-                        $match =  array_search($br['badge_id'], $filter_badge_id);
-                        if (!is_null($match) && $match !== false) { ?>
-                            <option selected="selected" value="<?php echo $br['badge_id']?>"><?php echo $br['name'];?></option>
-                        <?php }else{?>
-                            <option value="<?php echo $br['badge_id']?>"><?php echo $br['name'];?></option>
-                        <?php }?>
-                    <?php }?>
-                    </select>
-                </span>
-                <div>
-
-                </div>
             </div>
 
             <table class="list">
                 <thead>
                 <tr>
-                    <td width="40" class="left"><?php echo $this->lang->line('column_avatar'); ?></td>
-                    <td class="left"><?php echo $this->lang->line('column_player_id'); ?></td>
-                    <td class="left"><?php echo $this->lang->line('column_username'); ?></td>
-                    <td class="left"><?php echo $this->lang->line('column_email'); ?></td>
-                    <!-- <td class="left"><?php //echo $this->lang->line('column_level'); ?></td>
-                    <td class="left"><?php //echo $this->lang->line('column_exp'); ?></td> -->
-                    <td width="120" class="right"><?php echo $this->lang->line('column_reward_name'); ?></td>
-                    <td width="120" class="right"><?php echo $this->lang->line('column_reward_value'); ?></td>
-                    <td width="120" class="right"><?php echo $this->lang->line('column_date_added'); ?></td>
+                    <td class="left"><?php echo $this->lang->line('column_sender'); ?></td>
+                    <td class="left"><?php echo $this->lang->line('column_receiver'); ?></td>
+                    <td width="80" class="center"><?php echo $this->lang->line('column_gift_type'); ?></td>
+                    <td class="left"><?php echo $this->lang->line('column_gift_name'); ?></td>
+                    <td class="left"><?php echo $this->lang->line('column_gift_code'); ?></td>
+                    <td width="50" class="center"><?php echo $this->lang->line('column_gift_amount'); ?></td>
+                    <td width="120" class="left"><?php echo $this->lang->line('column_date_gifted'); ?></td>
                 </tr>
                 </thead>
                 <tbody>
                 <?php if ($reports) { ?>
                     <?php foreach ($reports as $report) { ?>
                     <tr>
-                        <td style="word-wrap:break-word;" class="left"><img width="40" height="40" src="<?php echo $report['image']; ?>" onerror="$(this).attr('src','<?php echo base_url();?>image/default-image.png');" /></td>
-                        <td style="word-wrap:break-word;" class="left"><?php echo $report['cl_player_id']; ?></td>
-                        <td style="word-wrap:break-word;" class="left"><?php echo $report['username']; ?></td>
-                        <td style="word-wrap:break-word;" class="left"><?php echo $report['email']; ?></td>
-                        <!-- <td class="left"><?php //echo $report['level']; ?></td>
-                        <td class="left"><?php //echo $report['exp']; ?></td> -->
-                        <td style="word-wrap:break-word;" class="right">
-                            <?php 
-                            if(isset($report['badge_name'])&&$report['badge_name']!=null){
-                                echo $report['badge_name'];
-                            }
-                            if(isset($report['reward_name'])&&$report['reward_name']!=null){
-                                echo $report['reward_name']['name'];
-                            }
-                            ?>
-                        </td>
-                        <td style="word-wrap:break-word;" class="right"><?php echo $report['value']; ?></td>
-                        <td style="word-wrap:break-word;" class="right"><?php echo $report['date_added']; ?></td>
+                        <td style="word-wrap:break-word;" class="left"><?php echo $report['sender']; ?></td>
+                        <td style="word-wrap:break-word;" class="left"><?php echo $report['receiver']; ?></td>
+                        <td style="word-wrap:break-word;" class="left"><?php echo $report['type']; ?></td>
+                        <td style="rword-wrap:break-word;" class="right"><?php echo $report['name']; ?></td>
+                        <td style="word-wrap:break-word;" class="right"><?php echo $report['code']; ?></td>
+                        <td style="word-wrap:break-word;" class="right"><?php echo $report['value'] ?></td>
+                        <td style="word-wrap:break-word;" class="right"><?php echo $report['date_gifted']; ?></td>
                     </tr>
                         <?php } ?>
                     <?php } else { ?>
                 <tr>
-                    <td class="center" colspan="8"><?php echo $text_no_results; ?></td>
+                    <td class="center" colspan="7"><?php echo $text_no_results; ?></td>
                 </tr>
                     <?php } ?>
                 </tbody>
@@ -121,10 +101,11 @@
         </div>
     </div>
 </div>
-<script type="text/javascript"><!--
+<script type="text/javascript">
 function filter() {
     var d = new Date().getTime();
-    url = baseUrlPath+'report_reward/reward_badge?t='+d;
+    // url = baseUrlPath+'report_reward/reward_badge?t='+d;
+    url = baseUrlPath+'report_gift/gift_filter?t='+d;
 
     var filter_date_start = $('input[name=\'filter_date_start\']').attr('value');
 
@@ -150,28 +131,17 @@ function filter() {
         url += '&username=' + encodeURIComponent(filter_username);
     }
 
-    var filter_badge_id = $('select[name=\'filter_badge_id\']').val();
+    var filter_gift_status = $('select[name=\'filter_gift_status\']').attr('value');
 
-    if (filter_badge_id != null) {
-        var badges = ""
-        filter_badge_id.forEach(function(element) {
-            if(badges == ""){
-                badges = encodeURIComponent(element);
-            } else {
-                badges += encodeURIComponent(',' + element);
-            }
-        });
-        if(badges != ""){
-            url += '&badge_id=' + badges;
-        }
+    if (filter_gift_status != 0) {
+        url += '&status=' + encodeURIComponent(filter_gift_status);
     }
-
     location = url;
 }
 
 function downloadFile() {
     var d = new Date().getTime();
-    url = baseUrlPath+'report_reward/actionDownload?t='+d;
+    url = baseUrlPath+'report_gift/giftDownload?t='+d;
 
     var filter_date_start = $('input[name=\'filter_date_start\']').attr('value');
 
@@ -197,29 +167,19 @@ function downloadFile() {
         url += '&username=' + encodeURIComponent(filter_username);
     }
 
-    var filter_badge_id = $('select[name=\'filter_badge_id\']').val();
+    var filter_gift_status = $('select[name=\'filter_gift_status\']').attr('value');
 
-    if (filter_badge_id != null) {
-        var badges = ""
-        filter_badge_id.forEach(function(element) {
-            if(badges == ""){
-                badges = encodeURIComponent(element);
-            } else {
-                badges += encodeURIComponent(',' + element);
-            }
-        });
-        if(badges != ""){
-            url += '&badge_id=' + badges;
-        }
+    if (filter_gift_status != 0) {
+        url += '&status=' + encodeURIComponent(filter_gift_status);
     }
 
     location = url;
 }
-//--></script>
+</script>
 <link id="bootstrap-style2" href="<?php echo base_url();?>javascript/bootstrap/chosen.min.css" rel="stylesheet">
 <script type="text/javascript" src="<?php echo base_url();?>javascript/bootstrap/chosen.jquery.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url();?>javascript/rule_editor/jquery-ui-timepicker-addon.js"></script>
-<script type="text/javascript"><!--
+<script type="text/javascript">
     $(document).ready(function() {
         $('#date-start').datetimepicker({dateFormat: 'yy-mm-dd',timeFormat: "HH:mm:ss"});
 
@@ -227,6 +187,7 @@ function downloadFile() {
     });
 
     $("#filter_timezone").chosen({max_selected_options: 1});
-    $("#filter_badge_id").chosen({max_selected_options: 5});
-//--></script>
+    $("#filter_goods_id").chosen({max_selected_options: 5});
+    $("#filter_goods_status").chosen({disable_search_threshold: 10});
+</script>
 
