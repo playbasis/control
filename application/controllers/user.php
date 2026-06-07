dbcb761372e43b1b2cc4ccf2de262e1179661a7d
RZForemost <robertz@foremost-ai.com>
2026-06-08 01:52:48 +0700

    Validate posted user group ids

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -195,6 +195,12 @@ class User extends MY_Controller
                 redirect('user/update/' . $user_id, 'refresh');
             }
 
+            $user_group = $this->input->post('user_group');
+            if ($user_group !== null && $user_group !== '' && !$this->isMongoId($user_group)) {
+                $this->session->set_flashdata('fail', $this->lang->line('text_fail'));
+                redirect('user/update/' . $user_id, 'refresh');
+            }
+
             //Check to see if it passes the form validation
 
             if ($this->form_validation->run()) {
@@ -254,6 +260,12 @@ class User extends MY_Controller
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             if (!$this->validateModify()) {
                 $this->session->set_flashdata('fail', $this->lang->line('error_permission'));
+                redirect('user/insert', 'refresh');
+            }
+
+            $user_group = $this->input->post('user_group');
+            if ($user_group !== null && $user_group !== '' && !$this->isMongoId($user_group)) {
+                $this->session->set_flashdata('fail', $this->lang->line('text_fail'));
                 redirect('user/insert', 'refresh');
             }
 
@@ -522,6 +534,11 @@ class User extends MY_Controller
         } else {
             return false;
         }
+    }
+
+    private function isMongoId($id)
+    {
+        return is_string($id) && preg_match('/^[0-9a-f]{24}$/i', $id) === 1;
     }
 
     private function checkOwnerUser($user_id)
