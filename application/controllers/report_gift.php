7b8338cc2b8cb3a2680fa9fd674cf2199db57c82
RZForemost <robertz@foremost-ai.com>
2026-06-08 01:39:11 +0700

    Validate goods and gift report time zones

--- application/controllers/report_gift.php
+++ application/controllers/report_gift.php
@@ -111,7 +111,7 @@ class Report_gift extends MY_Controller
         $UTC_7 = new DateTimeZone("Asia/Bangkok");
 
         if ($this->input->get('time_zone')){
-            $filter_time_zone = $this->input->get('time_zone');
+            $filter_time_zone = $this->getValidTimeZone($this->input->get('time_zone'));
             $parameter_url .= "&time_zone=" . urlencode($filter_time_zone);
             $newTZ = new DateTimeZone($filter_time_zone);
             $date_start = new DateTime( $filter_date_start, $newTZ);
@@ -249,6 +249,19 @@ class Report_gift extends MY_Controller
         }
     }
 
+    private function getValidTimeZone($time_zone)
+    {
+        $default_time_zone = "Asia/Bangkok";
+
+        if (!is_string($time_zone) || $time_zone === '') {
+            return $default_time_zone;
+        }
+
+        $time_zones = DateTimeZone::listIdentifiers(DateTimeZone::ALL);
+
+        return in_array($time_zone, $time_zones, true) ? $time_zone : $default_time_zone;
+    }
+
     public function giftDownload()
     {
         $parameter_url = "?t=" . rand();
@@ -286,7 +299,7 @@ class Report_gift extends MY_Controller
         $UTC_7 = new DateTimeZone("Asia/Bangkok");
         if ($this->input->get('time_zone')){
 
-            $filter_time_zone = $this->input->get('time_zone');
+            $filter_time_zone = $this->getValidTimeZone($this->input->get('time_zone'));
             $newTZ = new DateTimeZone($filter_time_zone);
             $date_start = new DateTime( $filter_date_start, $newTZ);
             $date_start->setTimezone($UTC_7);
