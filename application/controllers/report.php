35c2615e8b143d67c86e4807abdd6fe8b37f3e50
RZForemost <robertz@foremost-ai.com>
2026-06-08 03:28:22 +0700

    Validate action report date filters

--- application/controllers/report.php
+++ application/controllers/report.php
@@ -80,6 +80,39 @@ class Report extends MY_Controller
         $this->getActionList($offset, site_url('report/action_page'));
     }
 
+    private function getDefaultReportDateStart()
+    {
+        return date("Y-m-d H:i:s", strtotime(date("Y-m-d", strtotime("-7 days"))));
+    }
+
+    private function getDefaultReportDateEnd()
+    {
+        return date("Y-m-d H:i:s", strtotime(date("Y-m-d")) + 86399);
+    }
+
+    private function getReportDateFilter($value, $fallback, $end_of_day = false)
+    {
+        if (!is_string($value) || $value === '') {
+            return $fallback;
+        }
+
+        $value = trim($value);
+        if (!preg_match('/^\d{4}-\d{2}-\d{2}( \d{2}:\d{2}:\d{2})?$/', $value)) {
+            return $fallback;
+        }
+
+        $timestamp = strtotime($value);
+        if ($timestamp === false) {
+            return $fallback;
+        }
+
+        if ($end_of_day && strpos($value, '00:00:00') !== false) {
+            return date("Y-m-d H:i:s", $timestamp + 86399);
+        }
+
+        return $value;
+    }
+
     private function getActionList($offset, $url)
     {
         $offset = $this->input->get('per_page') ? $this->input->get('per_page') : $offset;
@@ -97,32 +130,24 @@ class Report extends MY_Controller
         $site_id = $this->User_model->getSiteId();
 
         if ($this->input->get('date_start')) {
-            $filter_date_start = $this->input->get('date_start');
+            $filter_date_start = $this->getReportDateFilter(
+                $this->input->get('date_start'),
+                $this->getDefaultReportDateStart()
+            );
             $parameter_url .= "&date_start=" . $filter_date_start;
         } else {
-            $date = date("Y-m-d", strtotime("-7 days"));
-            $previousDate = strtotime($date);
-            $filter_date_start = date("Y-m-d H:i:s", $previousDate);
+            $filter_date_start = $this->getDefaultReportDateStart();
         }
 
         if ($this->input->get('date_expire')) {
-            $filter_date_end = $this->input->get('date_expire');
+            $filter_date_end = $this->getReportDateFilter(
+                $this->input->get('date_expire'),
+                $this->getDefaultReportDateEnd(),
+                true
+            );
             $parameter_url .= "&date_expire=" . $filter_date_end;
-
-            if(strpos($filter_date_end, '00:00:00')){
-                //--> This will enable to search on the day until the time 23:59:59
-                $currentDate = strtotime($filter_date_end);
-                $futureDate = $currentDate + ("86399");
-                $filter_date_end = date("Y-m-d H:i:s", $futureDate);
-                //--> end*/
-            }
         } else {
-            //--> This will enable to search on the current day until the time 23:59:59
-            $date = date("Y-m-d");
-            $currentDate = strtotime($date);
-            $futureDate = $currentDate + ("86399");
-            $filter_date_end = date("Y-m-d H:i:s", $futureDate);
-            //--> end
+            $filter_date_end = $this->getDefaultReportDateEnd();
         }
 
         if ($this->input->get('time_zone')){
@@ -284,27 +309,22 @@ class Report extends MY_Controller
         $this->load->model('Player_model');
 
         if ($this->input->get('date_start')) {
-            $filter_date_start = $this->input->get('date_start');
+            $filter_date_start = $this->getReportDateFilter(
+                $this->input->get('date_start'),
+                $this->getDefaultReportDateStart()
+            );
         } else {
-            $date = date("Y-m-d", strtotime("-7 days"));
-            $previousDate = strtotime($date);
-            $filter_date_start = date("Y-m-d H:i:s", $previousDate);
+            $filter_date_start = $this->getDefaultReportDateStart();
         }
 
         if ($this->input->get('date_expire')) {
-            $filter_date_end = $this->input->get('date_expire');
-            if(strpos($filter_date_end, '00:00:00')){
-                //--> This will enable to search on the day until the time 23:59:59
-                $currentDate = strtotime($filter_date_end);
-                $futureDate = $currentDate + ("86399");
-                $filter_date_end = date("Y-m-d H:i:s", $futureDate);
-                //--> end*/
-            }
+            $filter_date_end = $this->getReportDateFilter(
+                $this->input->get('date_expire'),
+                $this->getDefaultReportDateEnd(),
+                true
+            );
         } else {
-            $date = date("Y-m-d");
-            $currentDate = strtotime($date);
-            $futureDate = $currentDate + ("86399");
-            $filter_date_end = date("Y-m-d H:i:s", $futureDate);
+            $filter_date_end = $this->getDefaultReportDateEnd();
         }
 
         if ($this->input->get('time_zone')){
