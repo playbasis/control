aa7ce77ad5eec48f84812cd3d56a93c785047ada
RZForemost <robertz@foremost-ai.com>
2026-06-08 10:25:56 +0700

    Guard language list tag shape

--- application/controllers/language.php
+++ application/controllers/language.php
@@ -206,6 +206,14 @@ class Language extends MY_Controller
 
 
         $this->data['languages'] = $this->Language_model->retrieveLanguage($filter);
+        foreach ($this->data['languages'] as &$language) {
+            if (isset($language['tags']) && is_string($language['tags'])) {
+                $language['tags'] = explode(',', $language['tags']);
+            } elseif (!isset($language['tags']) || !is_array($language['tags'])) {
+                $language['tags'] = array();
+            }
+        }
+        unset($language);
         $config['total_rows'] = $this->Language_model->getTotalLanguage($filter);
 
         $config['num_links'] = NUMBER_OF_ADJACENT_PAGES;
@@ -328,5 +336,10 @@ class Language extends MY_Controller
         } else {
             return false;
         }
+    }
+
+    private function isMongoId($id)
+    {
+        return is_string($id) && preg_match('/^[0-9a-f]{24}$/i', $id) === 1;
     }
 }
