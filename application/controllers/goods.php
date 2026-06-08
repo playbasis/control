343db6d6aa4e4800485ec78b620739d3fa54b694
RZForemost <robertz@foremost-ai.com>
2026-06-08 14:28:56 +0700

    Guard goods AJAX tag shape

--- application/controllers/goods.php
+++ application/controllers/goods.php
@@ -1289,6 +1289,13 @@ class Goods extends MY_Controller
                     }
                 }
 
+                $tags = isset($goods['tags']) ? $goods['tags'] : array();
+                if (is_string($tags) && $tags !== '') {
+                    $tags = explode(',', $tags);
+                } elseif (!is_array($tags)) {
+                    $tags = array();
+                }
+
                 $this->data['goods_list'][] = array(
                     'goods_id' => $goods['_id'],
                     'name' => $is_group ? $goods['group'] : $goods['name'],
@@ -1306,7 +1313,7 @@ class Goods extends MY_Controller
                     'sponsor' => isset($goods['sponsor']) ? $goods['sponsor'] : null,
                     'is_group' => $is_group,
                     'organize_name' => $org_name,
-                    'tags' => isset($goods['tags']) ? $goods['tags'] : null
+                    'tags' => $tags
                 );
             }
         }
