2b81fdb55aac8fe37118094a204113d4215ffb43
RZForemost <robertz@foremost-ai.com>
2026-06-10 18:28:04 +0700

    Fix PHP 8 Error class names

--- application/libraries/REST2_Controller.php
+++ application/libraries/REST2_Controller.php
@@ -42,7 +42,7 @@ abstract class REST2_Controller extends REST_Controller
         $this->load->model('rest_model');
         $this->load->model('auth_model');
         $this->load->model('client_model');
-        $this->load->model('tool/error', 'error');
+        $this->load->model('tool/error_model', 'error');
 
         /* 0.2 Adjust $this->request->body */
         if (!empty($this->request->body)) {
@@ -188,7 +188,7 @@ abstract class REST2_Controller extends REST_Controller
             call_user_func_array($method, $args);
         } catch (Exception $e) {
             ini_set('mongo.allow_empty_keys', TRUE);
-            $this->load->model('tool/error', 'error');
+            $this->load->model('tool/error_model', 'error');
             $msg = $e->getMessage();
             log_message('error', $msg);
             /* 3.2 Log response (exception) */
